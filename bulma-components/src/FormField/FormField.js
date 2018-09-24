fcd1c70bf06d802bfaba71b1d9d4447e4f393dc6
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-24 11:11:58 -0400

    Created formField

---
+++ bulma-components/src/FormField/FormField.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const formField = (props) => (
+        <div className="field">
+            <label className="label">{ props.label }</label>
+            <div className="control">
+                <input className="input" type={ props.type } placeholder={ props.placeholder } />
+            </div>
+        </div>
+);
+
+export default formField;
