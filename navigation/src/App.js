d741bf0f9ee270ce6a21b69cad69992273d548fa
alexrojas <alex.rojas@agintechllc.com>
2018-09-24 20:45:43 -0400

    CoolButton inside a button tag on Signup component

--- navigation/src/App.js
+++ navigation/src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar/>
         <Signup/>
 
 
--- navigation/src/Signup.js
+++ navigation/src/Signup.js
@@ -2,20 +2,50 @@ import React, { Component } from 'react';
 import './App.css'
 import Navbar from './Navbar'
 import FormField from './FormField.js'
+import CoolButton from './CoolButton'
 
 class Signup extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      isHidden : true
+    }
+  }
+  toggleHidden (){
+    this.setState({
+      isHidden: !this.state.isHidden
+    })
+  }
+
   render(){
     return(
       <div>
-        <Navbar/>
-        <FormField label="Name" type="text" placeholder="Alex Rojas"   />
-        <FormField label="Email" type="email" placeholder="Alex@gmail.com"   />
-        <FormField label="Password" type="password" placeholder="*******"   />
-      </div>
+         <button CoolButton class="button is-danger" buttonName="Log In" onClick={this.toggleHidden.bind(this)} >
+           Show Form!
+         </button>
+         {!this.state.isHidden && <Child />}
+       </div>
 
 
     )
   }
 }
+
+const Child = () => (
+
+  <div>
+    <form>
+
+      <FormField label="Name" type="text" placeholder="Alex Rojas"   />
+      <FormField label="Email" type="email" placeholder="Alex@gmail.com"   />
+      <FormField label="Password" type="password" placeholder="*******"   />
+
+    </form>
+
+  </div>
+)
+
+
+
 
 export default Signup
