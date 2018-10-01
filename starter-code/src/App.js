9510e3b31f2449c0fe66577dfc0ad41e05b18c42
wergilia <weronika.zdrojewska@gmail.com>
2018-10-01 17:05:57 +0200

    First commit

---
+++ starter-code/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar is-fixed-top"/>
+        
+        
+    )
+}
+
+// const Item = (props) => {
+//     return(
+//     <div> 
+//         <img src={props.url}/>
+//         <h3>{props.title}</h3>
+//         <p>{props.subtitle}</p>
+//     </div>)
+// }
+
+export const App = () => (
+        <div>
+            <Navbar/>
+            
+            
+            
+           
+        </div>
+    )
