dc430c36940ffedab245c8247346a46b95bbf884
Jhon Scarzo <jhon753951@gmail.com>
2018-10-01 23:42:58 +0200

    Iteracion 1 lista, stuck on 2

---
+++ starter-code/src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Navbar from './Navbar'
+import FormField from './FormField'
+
+
+const App = () => {
+    return (
+        <div>
+            {Navbar}
+            {/* {FormField} */}
+            {/* {CoolButton} */}
+            {/* {Message} */}
+        </div>
+    )
+}
+
+module.exports = App;
---
+++ starter-code/src/FormField.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const FormField = (
+    <div>
+        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+    </div>
+)
+
+module.exports = FormField;
