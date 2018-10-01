a0002ff63bc68f237b2cb3d2b5bdddd0d9214809
agdwm <almudenaguerraswm@gmail.com>
2018-10-01 23:19:56 +0200

    Fixed Nav Bar Burger

--- starter-code/src/navBar.js
+++ starter-code/src/navBar.js
@@ -9,12 +9,13 @@ export const NavBar = () => {
 				<a className="navbar-item" href="https://bulma.io">
 					<img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28"/>
 				</a>
+				<div className="navbar-burger burger" data-target="navbarExampleTransparentExample">
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
 			</div>
-			<div className="navbar-burger burger" data-target="navbarExampleTransparentExample">
-				<span></span>
-				<span></span>
-				<span></span>
-			</div>
+			
 
 			<div id="navbarExampleTransparentExample" className="navbar-menu">
 				<NavLink classNavStart="navbar-start" classNavItem="navbar-item" url="/" textLink="Home" />
