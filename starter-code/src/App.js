b4b47d435b8ca990bc48d5f7e33e0b4bbea912a0
wergilia <weronika.zdrojewska@gmail.com>
2018-10-01 23:42:55 +0200

    Navbar Component

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,11 +2,50 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <nav className="navbar is-fixed-top"/>
-        
-        
-    )
-}
+        <nav className="navbar is-transparent">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="https://bulma.io">
+                    <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
+                </a>
+                <div className="navbar-burger burger" data-target="navbarExampleTransparentExample">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+
+            <div id="navbarExampleTransparentExample" className="navbar-menu">
+                <div className="navbar-start">
+                    <a className="navbar-item" href="https://bulma.io/">
+                        Home
+                    </a>
+                </div>
+            </div>
+
+            <div className="navbar-end">
+                <div className="navbar-item">
+                    <div className="field is-grouped">
+                        
+                        <p className="control">
+                            <a className="bd-tw-button button" href="https://bulma.io/">
+                                
+                                <span>Login</span>
+                            </a>
+                        </p>
+                        <p className="control">
+                            <a className="button is-primary" href="https://bulma.io/">
+                                
+                                <span>Signup</span>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+</nav>
+
+
+            )
+        }
 
 // const Item = (props) => {
 //     return(
@@ -18,11 +57,11 @@ const Navbar = () => {
 // }
 
 export const App = () => (
-        <div>
-            <Navbar/>
-            
-            
-            
-           
-        </div>
-    )
+    <div>
+        <Navbar />
+
+
+
+
+    </div>
+)
