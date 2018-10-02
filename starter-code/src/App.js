4fb4f60d64e5e31632805ffe1eb36671eede02e1
yamioliva <yamioliva06@gmail.com>
2018-10-02 10:29:20 +0200

    Navbar

---
+++ starter-code/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Navbar} from "./navbar";
+
+export const App = () => {
+  return  (
+      <div style={{margin:"0"}}>
+          {Navbar}
+      </div>
+      
+  ) 
+}
