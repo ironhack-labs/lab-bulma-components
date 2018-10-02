af7810a1d9b9f34219d8b98aeb5ebfcc3e97b455
AdrianMelGon <adrianmelgon@gmail.com>
2018-10-02 09:47:55 +0200

    done

---
+++ starter-code/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <nav class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
+        </a>
+      </nav>
+
+    );
+  }
+}
+
+export default App;
