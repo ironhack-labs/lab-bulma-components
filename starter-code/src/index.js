780e4c95cd28d67a64553491501c8049d6e4dca0
Ruben Armendariz <rubenarmendarizcarretero@gmail.com>
2018-10-02 09:54:05 +0200

    DONE

---
+++ starter-code/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app.js";
+import 'bulma/css/bulma.css';
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
