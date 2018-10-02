bba7919d95fc8c8fb181e2455cd4d743250bd32d
pablotaboada <pablo.tabu@hotmail.com>
2018-10-02 09:56:05 +0200

    done

---
+++ starter-code/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./Navbar.js";
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      {/* <form>
+        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com"/>
+        <FormField label="Password" type="password" placeholder="····"/>
+        <CoolButton isPrimary className="is-rounded my-class">Signup</CoolButton>
+      </form> */}
+    </div>
+  );
+};
+
+export default App;
