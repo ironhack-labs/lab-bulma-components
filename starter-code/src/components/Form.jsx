acd5313400a493daac40e15ac5cb20a6274527d3
Eliot Cano <eliot7cano@gmail.com>
2018-10-02 09:48:24 -0500

    DE Mex Eliot Cano

---
+++ starter-code/src/components/Form.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+const Form = () =>{
+    return (
+        <div>
+            <form>
+            <div class="field">
+                <label class="label">Name</label>
+                <div class="control">
+                    <input label="Name" type="text" placeholder="e.g Alex Smith" />
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Name</label>
+                <div class="control">
+                    <input label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Password</label>
+                <div class="control">
+                    <input label="Password" type="password" placeholder="Password" />
+                </div>
+            </div>
+            </form>
+        </div>
+    )
+}
+
+
+export default Form
