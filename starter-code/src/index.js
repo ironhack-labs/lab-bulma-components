c3ed92d613e6e6890ec89ed2d682c13cf1961808
Jacob Casado <jacob.casado.perez@gmail.com>
2018-10-02 09:11:54 +0200

    Hecho hasta interacion 3

---
+++ starter-code/src/index.js
@@ -0,0 +1,13 @@
+import 'bulma/css/bulma.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import {App} from "./App.jsx";
+
+
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
