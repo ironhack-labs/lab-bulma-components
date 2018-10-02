5edbf8f73fd3e1a88ba9be86c2b6888f6896260b
omar-nav <omar.navarro@g.austincc.edu>
2018-10-01 23:06:02 -0500

    TODO add navbar with bulma

---
+++ starter-code/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+import "bulma/css/bulma.css";
+import Navbar from "./components/Navbar.jsx";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Nav /> <br />
+      </div>
+    );
+  }
+}
+
+export default App;
