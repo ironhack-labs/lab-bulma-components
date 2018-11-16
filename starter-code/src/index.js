86c84791c8e3137f4f363e3c58fb9802cee2b59d
Adolfo Vela <vela@tekplanner.com>
2018-11-15 21:33:34 -0600

    Listo antes de la iteracion 1

---
+++ starter-code/src/index.js
@@ -0,0 +1,9 @@
+ import React, { Component } from 'react'
+ import ReactDOM from "react-dom"
+ import Aff from "./App"
+ import 'bulma/css/bulma.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
