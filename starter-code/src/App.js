7ff4b78d9abf07f09f27965fa339492c90fbf33f
Adolfo Vela <vela@tekplanner.com>
2018-11-16 08:16:54 -0600

    Listo iteracion 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import Navbar from './Navbar';
+import FormField from './FormField';
 
 class App extends Component {
   render(){
     return(
       <div>
         <Navbar/>
+        <FormField/>
       </div>
     )
   }
