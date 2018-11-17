594dd06e512250a93f823241f189e55829ae3937
Pedro Cruz <pedro.cruz@blackinntech.com>
2018-11-16 21:46:45 -0600

    Iteration 2, 3 Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
 import Navbar from './Navbar';
+import FormField from './FormField';
+import CoolButton from './CoolButton';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Navbar/>
+                <div className="columns">
+                    <div className="column"></div>
+                    <div className="column is-one-third">
+                        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+                        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+                        <br />
+                        <CoolButton isSmall isDanger className="button is-danger is-rounded" name="Button 1"></CoolButton>
+                        <CoolButton isSmall isSuccess className="button is-success" name="Button 2"></CoolButton>
+                    </div>
+                    <div className="column"></div>
+                </div>
             </div>
         )
     }
---
+++ starter-code/src/CoolButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CoolButton = ({className, name}) => (
+    <button className={className}>{name}</button>
+);
+
+export default CoolButton;
