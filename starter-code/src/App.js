58d37c2f2a4ccf3e2f75590e5dcebf26c090357d
Schaum <schaumgarten@gmail.com>
2018-11-19 11:44:34 -0600

    iteración 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import Navbar from './Navbar';
+import FormField from "./FormField";
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Navbar/>
+                <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+                <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
             </div>
         );
     }
