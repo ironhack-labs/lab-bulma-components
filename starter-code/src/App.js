4a040b116f2f9a465801dba5e618cb33d7e16447
Schaum <schaumgarten@gmail.com>
2018-11-18 19:33:31 -0600

    primera iteración

---
+++ starter-code/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Navbar from './Navbar';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar/>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ starter-code/src/Navbar.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react'
+
+class Navbar extends Component {
+    render(){
+        return (
+            <div>
+                <nav className="navbar" role="navigation" aria-label="main navigation">
+                    <div className="navbar-brand">
+                        <a className="navbar-item" href="https://bulma.io">
+                            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
+                        </a>
+                        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false"
+                           data-target="navbarBasicExample">
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                        </a>
+                    </div>
+                    <div id="navbarBasicExample" className="navbar-menu">
+                        <div className="navbar-start">
+                            <a className="navbar-item">
+                                Home
+                            </a>
+                        </div>
+                    </div>
+                    <div class="navbar-end">
+                        <div class="navbar-item">
+                            <div class="buttons">
+                                <a class="button is-primary">
+                                    <strong>Sign up</strong>
+                                </a>
+                                <a class="button is-light">
+                                    Log in
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+    </nav>
+    </div>
+        )
+    }
+}
+
+
+export default Navbar;
