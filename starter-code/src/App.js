649484b3bb84d3ffb7467bd4b8bce56766a86914
N R <mnpuzzle@gmail.com>
2018-12-03 18:13:59 +0100

    first Iteration

---
+++ starter-code/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Nav from "./components/nav/nav.js"
+import "./components/nav/nav.css"
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+
+                <Nav></Nav>
+            </div>
+        );
+    }
+}
+
+export default App;
