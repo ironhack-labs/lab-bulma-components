f6e49e2fb622d208bae8d1a7d1de6d1f275999f7
Eduardo Gandía Fernández <eduardogandiafernandez@MacBook-Pro-de-Eduardo.local>
2018-12-03 18:48:30 +0100

    Iteration 1

---
+++ starter-code/src/components/navBar.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div className="navBar">
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <a className="navbar-item" href="https://bulma.io">
+              <img
+                src="https://bulma.io/images/bulma-logo.png"
+                width="112"
+                height="28"
+              />
+            </a>
+
+            <a
+              role="button"
+              className="navbar-burger burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+            </a>
+          </div>
+
+          <div id="navbarBasicExample" className="navbar-menu">
+            <div className="navbar-start">
+              <a className="navbar-item">Home</a>
+            </div>
+
+            <div class="navbar-end">
+              <div class="navbar-item">
+                <div class="buttons">
+                  <a class="button is-primary">
+                    <strong>Sign up</strong>
+                  </a>
+                  <a class="button is-light">Log in</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
