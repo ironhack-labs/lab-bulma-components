ff51e4ca9073109fb3b56cef4b86a377510fc18a
RuddyLotar <ruddylotar@gmail.com>
2018-12-03 17:37:23 +0100

    iteration 1

---
+++ starter-code/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./components/navbar/navbar.js";
+
+
+class App extends React.Component {
+  render() {
+    
+    return (
+      <div className="App">
+        <Navbar />
+               
+
+      </div>
+    );
+  }
+}
+
+export default App;
