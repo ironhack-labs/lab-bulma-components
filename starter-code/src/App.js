3b01bbb009a917d0d2dde2639a70dbe4320e6253
Zeswen <zeswentyrael@gmail.com>
2018-12-03 17:35:40 +0100

    Iteration 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 import Navbar from './components/Navbar/';
+import FormField from './components/FormField/';
+
 class App extends Component {
     render() {
         return(
             <React.Fragment>
                 <Navbar/>
+                <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+                <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
             </React.Fragment>
         )
     }
