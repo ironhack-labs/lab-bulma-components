a5443359faf11f90d1c1e878d02c4a3e58ca1c49
aterron84 <aterron84@gmail.com>
2018-12-03 19:38:25 +0100

    Iteration 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NavBar from "./components/navBar/navBar.jsx"
+import FormField from "./components/formField/formField.jsx"
 // import Header from "./components/header/header.jsx"
 // import Section from "./components/section/section.jsx"
 
@@ -8,6 +9,8 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar />
+        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
       </div>
     );
   }
