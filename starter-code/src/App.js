a15b9b764bea75e2e2d78a180e6dd3453dacb4e6
Yared Santiago <yaredmyers@gmail.com>
2018-12-03 18:15:07 +0100

    iteration 1

---
+++ starter-code/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import Nav from "../src/components/navbar/nav.js";
+
+class App extends Component {
+  render() {
+    return (
+      
+        <Nav />
+       
+    );
+  }
+}
+
+export default App;
