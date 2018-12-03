1fc4f6ca1d6d6ee81f4cf2c78fa3235c28b9a08b
sitobusnan <sito@citytonight.es>
2018-12-03 18:27:14 +0100

    it 3

---
+++ starter-code/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Navbar from './components/NavBar';
+import FormField from './components/FormField';
+import 'bulma/css/bulma.css';
+import '../public/styles/styles.css';
+
+class App extends React.Component {
+  render() {
+      return ( < div className = "App" >
+              <Navbar/>
+              <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+              <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+                 
+          </div>
+      );
+  }
+}
+
+export default App;
