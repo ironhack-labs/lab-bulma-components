f931b42b31bfa92f07a6a5c068808f6e8515275b
Bianca Castro <biiianca_00@hotmail.com>
2018-12-03 18:07:48 +0100

    iteration 1

---
+++ starter-code/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from './Components/Navbar/navbar.js'
+
+class App extends React.Component {
+
+render(){
+
+    return (
+        <div className="App">
+        <p>Prueba</p>
+        <Navbar />
+        </div>
+        
+    );
+}
+
+}
+
+export default App;
