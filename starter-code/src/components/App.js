7af05242591d64060c276d64d87f018a607f411d
Sonia Bravo <adeliadanjou@MacBook-Air-de-Sonia.local>
2018-12-03 18:35:33 +0100

    iteration 1

---
+++ starter-code/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import NavBar from "./Navbar/Navbar"
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <NavBar />
+      
+      </div>
+      
+    );
+  }
+}
+// exporta APP
+export default App;
