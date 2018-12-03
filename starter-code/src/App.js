c3e012381199674b9f7fb4e2894d5777164de1a6
PomPom248 <sofiroggero@gmail.com>
2018-12-03 18:35:03 +0100

    iteration 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import Nav from "../src/nav/nav.js"
-import "../src/nav/nav.css"
+import Form from "../src/form/form.js"
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-            <Nav></Nav>
-            {/* <h1>Hello</h1> */}
+                <Nav></Nav>
+                <Form></Form>
+                {/* <Form label="Name" type="text" placeholder="e.g Alex Smith" />
+                <Form label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" /> */}
             </div>
         );
     }
--- starter-code/src/nav/nav.js
+++ starter-code/src/nav/nav.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./nav.css";
 class Nav extends React.Component {
     constructor() {
         super()
