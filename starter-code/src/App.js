8919145e67f350375b92abf6ad6c4ad83d56f36b
cocobilbao <cocobilbao@gmail.com>
2018-12-03 18:37:20 +0100

    iteration 1

---
+++ starter-code/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+import  Navbar from "./components/navbar.js"
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        
+        <Navbar/>
+       
+      </div>
+    );
+  }
+}
+
+export default App;
