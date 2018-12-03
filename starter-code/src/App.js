93dc85f6f69bf8493180f6163264ca2f22cf9256
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-12-03 19:27:39 +0100

    Iteration 1

---
+++ starter-code/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar.js";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Navbar/>
+            </div>
+        );
+    }
+}
+
+export default App;
