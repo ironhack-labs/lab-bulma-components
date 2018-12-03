cddd221765d6977e7a22462b41b11a1574a12e16
Jorge Devesa <jrgdevesa@gmail.com>
2018-12-03 20:48:47 +0100

    Iteration 3 semi done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import NavBar from "./Components/navbar/navbar.jsx"
 import FormField from "./Components/FormField/formfield"
+
 // import "./App.css";
 
 
@@ -11,6 +12,7 @@ class App extends Component {
        <NavBar />
        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+
       </div>
 
     );
--- starter-code/src/Components/navbar/navbar.jsx
+++ starter-code/src/Components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import CoolButton from "../coolButton/coolButton"
 
 
 class NavBar extends Component{
@@ -56,22 +56,14 @@ class NavBar extends Component{
       <div className="navbar-item">
         <div className="field is-grouped">
           <p className="control">
-            <a className="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="http://localhost:4000" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=http://localhost:4000&amp;via=jgthms">
-              <span className="icon">
-                <i className="fab fa-twitter"></i>
-              </span>
-              <span>
-                Login
-              </span>
-            </a>
+   
+
+
+              <CoolButton className="button is-rounded my-class is-danger is-small">Button 1</CoolButton>
+
           </p>
           <p className="control">
-            <a className="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.7.2/bulma-0.7.2.zip">
-              <span className="icon">
-                <i className="fas fa-download"></i>
-              </span>
-              <span>Signup</span>
-            </a>
+          <CoolButton className="button is-small is-success">Button 2</CoolButton>
           </p>
         </div>
       </div>
