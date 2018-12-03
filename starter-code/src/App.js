a5b1c1587a1bd05262f8d59f7c9eee0d662723e7
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-12-03 20:16:47 +0100

    Iteration 4

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar.js";
-import FormField from "./components/FormField/FormField.js";
-import CoolButton from "./components/CoolButton/CoolButton.js";
+import Signup from "./components/Signup/Signup.js";
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                <Navbar />
-                <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
-                <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
-                <CoolButton isSmall isDanger className="is-rounded my-class">Button 1</CoolButton>
-                <CoolButton isSmall isSuccess>Button 2</CoolButton>
+                <Signup />
             </div>
         );
     }
