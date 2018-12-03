4874bdf72adbb3541a5290dffac963a8494940bf
aterron84 <aterron84@gmail.com>
2018-12-03 18:57:51 +0100

    Iteration 1

---
+++ starter-code/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import NavBar from "./components/navBar/navBar.jsx"
+// import Header from "./components/header/header.jsx"
+// import Section from "./components/section/section.jsx"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+      </div>
+    );
+  }
+}
+
+export default App;
