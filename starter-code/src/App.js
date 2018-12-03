7bd67e54f36fac9b7ff0f4bb9b4ee98bb31d58e6
Sonia Ruiz <yune.vk@gmail.com>
2018-12-03 17:50:07 +0100

    Iteration 2 | FormField Component

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 
 import NavBar from "./components/NavBar/NavBar";
+import FormField from "./components/FormField/FormField";
 
 class App extends Component {
   render() {
@@ -9,6 +10,9 @@ class App extends Component {
       <div className="App">
         <NavBar />
         <h1> Hello Ironhackers! </h1>
+
+        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
       </div>
     );
   }
