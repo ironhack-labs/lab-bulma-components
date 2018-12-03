219573296bd0a6d982c59cd7c30ec4265edc6f75
Xiaomi Notebook Pro <xiaomi@gmailcom>
2018-12-03 17:26:12 +0100

    first interation done

---
+++ starter-code/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Navbar from './components/Navbar';
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+        <React.Fragment>
+            <Navbar></Navbar>
+        </React.Fragment>
+     
+    );
+  }
+}
+
+export default App;
