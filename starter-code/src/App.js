9ce7ba2e744db8255fb9a2c78f37160ea9670a82
Jaime <herrerotajaime@gmail.com>
2018-12-04 08:31:58 +0100

    navbar

---
+++ starter-code/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Signup from "../src/signup/signup"
+ class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Signup></Signup>
+            </div>
+        );
+    }
+}
+ export default App; 
