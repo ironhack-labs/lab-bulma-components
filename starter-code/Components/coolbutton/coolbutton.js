304e4e796a89f97941ac9be35379a39a10e076a3
adrigt00 <adriangomez@MacBook-Pro-de-Adrian.local>
2018-12-04 08:42:40 +0100

    3 iteracion

---
+++ starter-code/Components/coolbutton/coolbutton.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+
+class coolbutton extends Component {
+
+  render() {
+    return (
+      <div className="coolbutton">
+  <button class="button is-rounded my-class is-danger is-small">Button 1</button>
+  <button class="button is-small is-success">Button 2</button>
+</div>
+    )}
+}
+
+export default coolbutton;
--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "../Components/navbar/Navbar.js";
 import FormField from "../Components/formfield/formfield";
+import CoolButton from "../Components/coolbutton/coolbutton";
 class App extends Component {
   render() {
     return (<div className="App">
@@ -10,6 +11,8 @@ class App extends Component {
     </header>
     <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
     <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" /> 
+    {/* <CoolButton isSmall isDanger className="is-rounded my-class">Button 1</CoolButton> */}
+    <CoolButton isSmall isSuccess>Button 2</CoolButton>
     </div>)
     }
 }
