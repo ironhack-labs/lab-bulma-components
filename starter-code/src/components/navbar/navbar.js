72ded4c6a7a64805ec971a953b737595f99a5468
David Diez <ddiezr@gmail.com>
2018-12-04 09:38:06 +0100

    Added Cool Button in the navbar as a component

--- starter-code/src/components/navbar/navbar.js
+++ starter-code/src/components/navbar/navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Navbar.css";
+import CoolButton from '../coolButton/coolButton';
 
 
 class Navbar extends React.Component {
@@ -34,32 +35,12 @@ class Navbar extends React.Component {
           <div className="navbar-end">
             <div className="navbar-item">
               <div className="field is-grouped">
-                <p className="control">
-                  <a
-                    className="bd-tw-button button"
-                    data-social-network="Twitter"
-                    data-social-action="tweet"
-                    data-social-target="http://localhost:4000"
-                    target="_blank"
-                    href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=http://localhost:4000&amp;via=jgthms"
-                  >
-                    <span className="icon">
-                      <i className="fab fa-twitter" />
-                    </span>
-                    <span>Login</span>
-                  </a>
-                </p>
-                <p className="control">
-                  <a
-                    className="button is-primary"
-                    href="https://github.com/jgthms/bulma/releases/download/0.7.2/bulma-0.7.2.zip"
-                  >
-                    <span className="icon">
-                      <i className="fas fa-download" />
-                    </span>
-                    <span>Signup</span>
-                  </a>
-                </p>
+                <div className="control">
+                <CoolButton isSmall isDanger className="is-rounded my-class">Login</CoolButton>
+                </div>
+                <div className="control">
+                <CoolButton isSmall isSuccess>Sign Up</CoolButton>
+                </div>
               </div>
             </div>
           </div>
--- starter-code/src/components/signup/signup.js
+++ starter-code/src/components/signup/signup.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'    
 import Navbar from '../navbar/navbar';
-import FormField from '../formField/formField'
-import CoolButton from '../coolButton/coolButton';
+import FormField from '../formField/formField';
 import Message from '../message/message';
 
 export default class Signup extends Component {
@@ -13,8 +12,6 @@ export default class Signup extends Component {
         <Navbar/>
         <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
         <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
-        <CoolButton isSmall isDanger className="is-rounded my-class">Button 1</CoolButton>
-        <CoolButton isSmall isSuccess>Button 2</CoolButton>
        <Message isInfo title="Hello World">
          Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>.
        </Message>
