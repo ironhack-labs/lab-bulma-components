5d993781592e05151e5f2debf4aefcdc9efd950f
Teresa Romero <tromerolozano@gmail.com>
2019-01-10 21:22:41 +0100

    started lab

---
+++ starter-code/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar"
+import FormField from "./components/FormField"
+import CoolButton from "./components/CoolButton"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+        <CoolButton isSmall isDanger className="is-rounded my-class">Button 1</CoolButton>
+        <CoolButton isSmall isSuccess>Button 2</CoolButton>
+      </div>
+    );
+  }
+}
+
+export default App;
