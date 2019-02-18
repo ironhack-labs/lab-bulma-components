569a702955193c38ad19ac6c79dff2e6894e0097
Raphael Huber <raphael.ehuber@gmail.com>
2019-02-18 18:30:26 -0300

    first iteration done

---
+++ starter-code/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Navbar from "./Navbar.js";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+      </div>
+    );
+  }
+}
+
+export default App;
