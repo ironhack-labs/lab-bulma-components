a018c32b75981ea3ff4fc1fa79d6bc47dcf0a4fe
Sofía <sofianombela@gmail.com>
2019-02-18 17:51:14 +0100

    iteration 2 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
-import Navbar from "../src/Navbar"
+import Navbar from "../src/Navbar";
+import FormField from "../src/FormField";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar/>
+        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+
       </div>
     );
   }
