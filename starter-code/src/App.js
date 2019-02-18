bbb1e4f84aa8ee1218464340894db894939b482c
joao pedro <jp_serrat@hotmail.com>
2019-02-18 19:25:25 -0300

    done

---
+++ starter-code/src/App.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react"
+
+import Nav from "./Nav.js"
+import FormField from "./FormField.js"
+import CoolButton from "./CoolButton.js"
+import Signup from "./Signup.js"
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+
+        <Nav/>
+
+        <Signup/>
+
+
+
+      </div>
+    );
+  }
+
+}
+
+
+
+export default App;
---
+++ starter-code/src/Signup.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react"
+import Nav from "./Nav.js"
+import FormField from "./FormField.js"
+import CoolButton from "./CoolButton.js"
+
+class Signup extends Component {
+  render(){
+    return(
+      <form>
+          <FormField name = "name"/>
+          <FormField name = "email" />
+          <FormField name = "password"/>
+          <CoolButton name = "sign up" isSmall isDanger className="is-rounded my-class"/>
+      </form>
+    )
+  }
+
+}
+
+export default Signup 
