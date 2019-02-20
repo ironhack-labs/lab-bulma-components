11d4fa5810ab5aa2870cfce1a7716ef2780de931
Lucia Cazorla <a.lucia.cazorla@gmail.com>
2019-02-20 21:11:26 +0100

    feat: Navbar added.

---
+++ starter-code/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+
+export default class App extends Component {
+  render(){
+    return(
+      <Navbar />
+    );
+  }
+}
