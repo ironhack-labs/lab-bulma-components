5ac39dca166773eda32deed0d95c435f73ce50d0
Moises Garcia <moisesgarcia83@gmail.com>
2019-02-20 21:05:25 +0100

    feat: navbar component

---
+++ starter-code/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+
+export default class App extends Component {
+
+  
+  render() {
+    return (
+      
+      <Navbar />
+     
+    )
+  }
+
+}
+
