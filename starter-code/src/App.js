ab5fd11c81abd5dfeb2371fd338ab158b29784e9
Vero Celemin <veronica.celemin@gmail.com>
2019-02-21 10:33:33 +0100

    feat: Iteration 1 done -navigation bar-

---
+++ starter-code/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+
+export default class App extends Component {
+
+  render() {
+    return(
+      <Navbar/>
+    );
+  }
+}
