49819292db4800a0e7f3e8702cde437d511d42ab
Vero Celemin <veronica.celemin@gmail.com>
2019-02-21 12:09:11 +0100

    feat: Iteration 2 done -Form Field

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
+import FormField from './components/FormField';
 
 export default class App extends Component {
 
+  user = {
+    name: 'Vero',
+    lastName: 'Lominchar',
+    age: 37
+  }
+
+
+
   render() {
     return(
-      <Navbar/>
+      <div className="App">
+        <Navbar/>
+        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+      </div>
     );
   }
 }
