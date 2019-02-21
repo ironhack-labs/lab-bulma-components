2ce90ed75e03b96a2e3f09fc64d24676a19f878e
Arthur Houdebine <arthur.houdebine@gmail.com>
2019-02-21 21:40:31 +0100

    finished iteration 2

---
+++ starter-code/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Navbar from "./Navbar.js";
+import FormField from "./FormField.js";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+      </div>
+    );
+  }
+}
+
+export default App;
