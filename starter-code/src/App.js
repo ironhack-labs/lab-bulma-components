be5f95eb1b0705ccc2cc6400f60fcac15de345d2
Rodrigo Otero <roterom82@gmail.com>
2019-02-22 20:02:18 +0100

    feat: fieldForm component done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import NavBar from "./components/NavBar";
+import FormField from "./components/FormField";
 
 
 export default class App extends Component {
 
   render() {
     return (
-      <NavBar />
+      <div>
+        <NavBar />
+        <FormField label={"Name"} type="text" placeholder={"e.g Alex Smith"} />
+        <FormField label={"Email"} type="email" placeholder={"e.g. alexsmith@gmail.com"} />
+      </div>
     )
   }
 }
---
+++ starter-code/src/components/FormField.js
@@ -0,0 +1,19 @@
+import React,  { Component } from "react";
+
+export default class FormField extends Component {
+
+  constructor (props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className="field">
+        <label className="label">{this.props.label}</label>
+        <div className="control">
+          <input className="input" type="text" placeholder={this.props.placeholder}/>
+        </div>
+      </div>
+    )
+  }
+}
