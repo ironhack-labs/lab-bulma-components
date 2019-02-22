7605e371a163dac4ae0f5e7333e47758f683a3d2
Rodrigo Otero <roterom82@gmail.com>
2019-02-22 16:37:43 +0100

    feat: navBar done

---
+++ starter-code/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <NavBar />
+    )
+  }
+}
+
