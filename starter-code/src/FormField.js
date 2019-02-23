cdf8898f1aada2074b0b063e7637bdc80c296803
justinezahiri <justinezahiri@gmail.com>
2019-02-23 10:28:08 +0100

    iteration2 completed

--- starter-code/src/FormField.js
+++ starter-code/src/FormField.js
@@ -3,14 +3,17 @@ import React from "react";
 class FormField extends React.Component { 
   render() {
 
-    const {label, type, placeholder} = this.props;
+    //const {label, type, placeholder} = this.props;
 
     return (
-      <div className="FormField">
-      <p>{label}</p>
-      </div>
+      <div className="field">
+      <label className="label">{this.props.label}</label>
+      <div className="control">
+      <input className="input" type={this.props.type} placeholder={this.props.placeholder}></input></div></div>
     );
   }
 } 
+
+
 
 export default FormField;
