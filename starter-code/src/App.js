4e8f26c5d90a738a5560bab821c674a51c22d4fc
Nerea <nereaparicio@gmail.com>
2019-02-26 19:53:13 +0100

    FormField Component

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
+import Form from './components/Form'
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <spam></spam>
+        <div className="main container">
+          <div className="container">
+            <Form label="Name" type="text" placeholder="e.g Alex Smith" />
+            <Form label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+          </div>
+        </div>
       </div>
     );
   }
