9110349bdc16b95478d4002b2ddf710b14fcea5f
CGuillotG <c.guillot@outlook.com>
2019-02-26 09:33:14 -0600

    Not done

---
+++ starter-code/src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import Navbar from "./components/Navbar.jsx"
+// import Section1 from "./components/Section1"
+// import Section2 from "./components/Section2"
+
+
+class App extends Component {
+    render(){
+        return (
+            <div>
+                <Navbar />
+                <h1>Test</h1>
+            </div>
+        )
+    }
+}
+
+
+export default App
---
+++ starter-code/src/components/FormField.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => {
+  return (
+        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+  )
+}
