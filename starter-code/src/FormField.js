a6aee9e2d4c9afb0504c5136f37ca8d6e524b8e7
ernestogl88 <ernestogomezlopez@gmail.com>
2019-04-29 19:29:55 +0200

    Finish

---
+++ starter-code/src/FormField.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class FormField extends React.Component {
+  render() {
+    return (
+      <div className="field">
+        <label className="label">{this.props.label}</label>
+        <div className="control">
+          <input className="input" type={this.props.type} placeholder={this.props.placeholder} />
+        </div>
+      </div>
+    );
+  }
+}
