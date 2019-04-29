debd8544a4ec1b86155657ebe14eb0c25c8a1470
fcovacarh <fcovacarh@gmail.com>
2019-04-29 18:58:38 +0200

    Initial code and Bulma navbar component created

---
+++ starter-code/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BulmaNav from "./BulmaNav.jsx";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div id="app-container">
+      <BulmaNav />
+      </div>
+    );
+  }
+}
+
+export default App;
