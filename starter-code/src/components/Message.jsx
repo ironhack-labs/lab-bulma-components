a45918fe6621338f17c4501f2bb649f39c54d821
Luca Manfredi <luca.manfredi1987@gmail.com>
2019-04-30 09:25:48 +0200

    done

--- starter-code/src/components/Message.jsx
+++ starter-code/src/components/Message.jsx
@@ -1,48 +1,11 @@
 import React, { Component } from 'react'
+import checkAttribute from "../helper/checkAttribute"
 
 export default class Message extends Component {
-  render() {
-    const classes = {
-        isActive: 'is-active',
-        isBlack: 'is-black',
-        isCentered: 'is-centered',
-        isDanger: 'is-danger',
-        isDark: 'is-dark',
-        isFocused: 'is-focused',
-        isGrouped: 'is-grouped',
-        isHovered: 'is-hovered',
-        isInfo: 'is-info',
-        isInverted: 'is-inverted',
-        isLarge: 'is-large',
-        isLight: 'is-light',
-        isLink: 'is-link',
-        isLoading: 'is-loading',
-        isMedium: 'is-medium',
-        isOutlined: 'is-outlined',
-        isPrimary: 'is-primary',
-        isRight: 'is-right',
-        isRounded: 'is-rounded',
-        isSelected: 'is-selected',
-        isSmall: 'is-small',
-        isStatic: 'is-static',
-        isSuccess: 'is-success',
-        isText: 'is-text',
-        isWarning: 'is-warning',
-        isWhite: 'is-white',
-      }
-      let classNameString = this.props.className;
-
-      Object.keys(this.props).forEach((el) => {
-        if (Object.keys(classes).indexOf(el) >= 0) {
-          classNameString = `${classes[el]}`;
-        }
-      });
-
-      classNameString = 'message ' + classNameString;
-      
+  render() {     
     return (
       <div className="messageBox">
-        <article className={classNameString}>
+        <article className={checkAttribute('message', this.props)}>
             <div className="message-header">
                 <p>{this.props.title}</p>
                 <button className="delete" aria-label="delete"></button>
@@ -53,3 +16,4 @@ export default class Message extends Component {
     )
   }
 }
+
---
+++ starter-code/src/helper/checkAttribute.js
@@ -0,0 +1,43 @@
+let checkAttribute = (element, props) => {
+
+    const classes = {
+        isActive: 'is-active',
+        isBlack: 'is-black',
+        isCentered: 'is-centered',
+        isDanger: 'is-danger',
+        isDark: 'is-dark',
+        isFocused: 'is-focused',
+        isGrouped: 'is-grouped',
+        isHovered: 'is-hovered',
+        isInfo: 'is-info',
+        isInverted: 'is-inverted',
+        isLarge: 'is-large',
+        isLight: 'is-light',
+        isLink: 'is-link',
+        isLoading: 'is-loading',
+        isMedium: 'is-medium',
+        isOutlined: 'is-outlined',
+        isPrimary: 'is-primary',
+        isRight: 'is-right',
+        isRounded: 'is-rounded',
+        isSelected: 'is-selected',
+        isSmall: 'is-small',
+        isStatic: 'is-static',
+        isSuccess: 'is-success',
+        isText: 'is-text',
+        isWarning: 'is-warning',
+        isWhite: 'is-white',
+    }
+    
+    let classNameString = props.className ? props.className : "";
+
+    Object.keys(props).forEach((el) => {
+        if (Object.keys(classes).indexOf(el) >= 0) {
+        classNameString += classNameString.split(' ').length > 0 ? ` ${classes[el]}` : `${classes[el]}`;
+        }
+    });
+
+  return classNameString = element + ' ' + classNameString;
+}
+
+module.exports = checkAttribute
