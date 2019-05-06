32290a9e30f1e6be4614c7fcad2ed47e8b0c55b4
Arthur <arthur_sbd@hotmail.com>
2019-05-06 17:51:48 -0300

    navbar

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-
+import Navbar from './Navbar';
 
 class App extends Component {
   render() {
     return (
         <div className="App">
+          <Navbar />
           <h1> Hello Ironhackers! </h1>
         </div>
     );
