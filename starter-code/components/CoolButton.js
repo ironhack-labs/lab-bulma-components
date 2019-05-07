617db1748e1d4af30cc057c5f426add62b02dbba
JeffersonISLima <38190813+JeffersonISLima@users.noreply.github.com>
2019-05-06 22:02:57 -0300

    Update CoolButton.js

--- starter-code/components/CoolButton.js
+++ starter-code/components/CoolButton.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
-function CoolButton(props) {
-  /* Caso exista dois botões com classes diferentes, usar essa condição, cada btn fica com sua formatação
-    if (props.isSmall && props.isSuccess) {
-    return <button className="button is-large">{props.children}</button>;
-    } */
+function CoolButton(props) {  
   return <button className={props.className}>{props.children}</button>;
 }
 
