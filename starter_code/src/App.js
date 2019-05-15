97d536ead6a4554feb5522fc4fbfbe0ee75c97e9
Nathaniel Carvajal <nathanielcarvajal@gmail.com>
2019-05-14 20:49:35 -0400

    navbar and form

---
+++ starter_code/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import 'bulma/css/bulma.css';
+import Navbar from "./Navbar";
+import Formfield from "./Formfield";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Formfield/>
+    </div>
+  );
+}
+
+export default App;
