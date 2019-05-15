72f1258500aeb4e22b471a9399a4125ece583377
Mario Neto <netto.mtx@gmail.com>
2019-05-15 09:54:54 -0300

    almost

---
+++ starter-code/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Forms from './Forms';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Navbar />
+      );
+  }
+}
+
+export default App;
---
+++ starter-code/src/Forms.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Forms = () => {
+  return (
+    <h1>helloooo</h1>
+  )
+
+}
+
+
+
+
+
+
+
+
+
+export default Forms;
