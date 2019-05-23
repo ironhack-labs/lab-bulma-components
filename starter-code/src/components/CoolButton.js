8c1be9682bb18e691e61a4f709fca3a2326ebc8b
MartaVaqgut <martavaquerogutierrez@gmail.com>
2019-05-23 19:07:20 +0200

    iteraciones1, 2, 3, y 4

---
+++ starter-code/src/components/CoolButton.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CoolButton = (props) => {
+  const {isSmall, isDanger, className, isSuccess, children} = props;
+  const classNameString = `
+  button
+  ${className}
+  ${isSmall ? 'is-small' : ''}
+  ${isDanger ? 'is-danger' : ''}
+  ${isSuccess ? 'is-success' : ''}
+  `
+
+   return(
+    <button className={classNameString}>
+      {children}
+    </button>
+    )
+};
+
+export default CoolButton 
