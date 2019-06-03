0d6bbd89e93bae3874bd044a4bb3576c808e7342
Marnelas <marnelasbenito@gmail.com>
2019-06-03 18:47:36 +0200

    bulma buttons added

---
+++ starter-code/components/CoolButton.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const CoolButton = props => <a className={props.class}>{props.value}</a>
+
+
+
+
+
+
+
+export default CoolButton;
--- starter-code/src/app.js
+++ starter-code/src/app.js
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 import NavBar from "../components/NavBar"
+import FormField from "../components/FormField"
+import "bulma/css/bulma.css";
+
+
 import "../src/App.css"
+import CoolButton from "../components/CoolButton";
 
 
 class App extends Component {
 
 render () {
   return (
-    <div className= "App">
+    <div className="App">
       <NavBar />
       <h1>bulma App</h1>
+
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
+      <CoolButton class="button is-danger is-medium" value="button1" />
+      <CoolButton class="button is-success is-medium" value="button2" />
     </div>
   );
 }
