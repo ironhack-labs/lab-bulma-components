6037e5e26cb456828d177487ac904a731244ac15
Lucia Astray <lu@MacBook-Pro-de-Lucia.local>
2019-06-03 17:54:42 +0200

    Iterations 1

---
+++ starter-code/src/app.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import Navbar from '../components/navbar'
+
+class App extends Component {
+  
+  render() {
+
+    return (
+
+      <Navbar />
+    )
+  }
+}
+
+export default App
