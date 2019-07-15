45ecd7804a8ab1d3d68ec13da7b6298bcec22b55
Francho Garcia <franchog79@gmail.com>
2019-07-15 22:39:37 +0200

    Finished first iteration

---
+++ starter-code/src/NavbarBulma.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+export default class NavbarBulma extends React.Component {
+  render() {
+    return (
+      <nav className="navbar is-transparent">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
+            <img
+              src="https://bulma.io/images/bulma-logo.png"
+              alt="Bulma: a modern CSS framework based on Flexbox"
+              width="112"
+              height="28"
+            />
+          </a>
+          <div
+            className="navbar-burger burger"
+            data-target="navbarExampleTransparentExample"
+          >
+            <span />
+            <span />
+            <span />
+          </div>
+        </div>
+
+        <div id="navbarExampleTransparentExample" className="navbar-menu">
+          <div className="navbar-start">
+            <a className="navbar-item" href="https://bulma.io/">
+              Home
+            </a>
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <a className="button is-rounded my-class is-danger is-small">
+                  <strong>Sign up</strong>
+                </a>
+                <a className="button is-small is-success">Log in</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
