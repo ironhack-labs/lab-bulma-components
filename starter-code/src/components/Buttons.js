2ad282b391ccb1fe261df2e6f7c500a618596b80
Gabriel B <gabelmo92@hotmail.com>
2019-07-15 20:14:54 +0200

    done2

--- starter-code/src/components/Buttons.js
+++ starter-code/src/components/Buttons.js
@@ -4,12 +4,12 @@ export default class Buttons extends Component {
   render() {
     return (
       <div className="buton">
-        <article class="message is-info">
-          <div class="message-header">
+        <article className="message is-info">
+          <div className="message-header">
             <p>Web Under Maintenance</p>
-            <button class="delete" aria-label="delete" />
+            <button className="delete" aria-label="delete" />
           </div>
-          <div class="message-body">
+          <div className="message-body">
             The submit button is under maintenance, please wait a moment for the 
             developer to fix it.
           </div>
