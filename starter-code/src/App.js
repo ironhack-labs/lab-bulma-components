769e00ebdd3010b6e8a51712a2216694db76f267
Francho Garcia <franchog79@gmail.com>
2019-07-15 23:08:42 +0200

    Iteration 2 finished

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import NavbarBulma from "./NavbarBulma";
+import FormField from "./FormField";
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <NavbarBulma />
+        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+        <FormField
+          label="Email"
+          type="email"
+          placeholder="e.g. alexsmith@gmail.com"
+        />
       </div>
     );
   }
---
+++ starter-code/src/FormField.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export default class FormField extends Component {
+  render() {
+    return (
+      <div className="formfield">
+        <div className="field">
+          <label className="label">{this.props.label}</label>
+          <div className="control">
+            <input
+              className="input"
+              type={this.props.type}
+              placeholder={this.props.placeholder}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
