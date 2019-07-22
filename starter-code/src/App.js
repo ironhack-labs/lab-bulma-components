94b8b9c9ba4b013137764b4b6c1250483c84e68e
giovannipc <giovannipaulocunha@hotmail.com>
2019-07-22 19:14:08 -0300

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import SignUp from './components/signUp';
+// import Navbar from './components/navbar';
+// import FormField from './components/formField';
+// import CoolButton from './components/coolButton';
+import SignUp from './components/signUp'
 import "./App.css";
 
 class App extends Component {
@@ -9,5 +12,18 @@ class App extends Component {
     );
   }
 }
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <Navbar />
+//         <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+//         <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+//         <CoolButton isSmall isDanger className="is-rounded my-class">Button 1</CoolButton>
+//         <CoolButton isSmall isSuccess>Button 2</CoolButton>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
