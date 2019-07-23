491bfab371f9b5c6f33eaca152f1a01e6a768f31
Victor Levy <victorlevygp@gmail.com>
2019-07-23 09:58:00 -0300

    corrected container

--- starter-code/src/components/Signup.js
+++ starter-code/src/components/Signup.js
@@ -1,12 +1,23 @@
 import React, {Component, Fragment} from "react";
 import Nav from './Nav.js';
 import Container from './Container.js';
+import FormField from './FormField.js';
+import CoolButton from './CoolButton.js';
+import Message from './Message.js';
 
 const Signup = () => {
   return (
     <Fragment>
       <Nav />
-      <Container />
+      <Container>
+        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+        <FormField label="Password" type="password" />
+        <CoolButton isSmall isSuccess className="is-rounded my-class">Submit</CoolButton>
+        <Message isInfo title="Hello World">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>.
+        </Message>
+      </Container>
     </Fragment>
   )
 }
