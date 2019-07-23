621b07b444ef67718180c4bc48e3e48491c84b23
Taluama Velludo <taluama@gmail.com>
2019-07-22 21:02:33 -0400

    buttons done

--- starter-code/src/components/CoolButton/CoolButton.js
+++ starter-code/src/components/CoolButton/CoolButton.js
@@ -5,23 +5,14 @@ class CoolButton extends Component {
   render(props) {
     return (
       <div className="CoolButton">
-        <p>
-          {/* {this.props.className} */}
-        </p>
-        <p>
-          {this.props.children}
-        </p>
-        {/* <button className="{this.props.className}" class="button is-danger is-small">Button 1</button> */}
-        {/* <button class="button is-small is-success">Button 2</button> */}
-      </div>
-    );
-  }
-}
-
-export default CoolButton;
-
-
-
-
-{/* <CoolButton isSmall isDanger className="is-rounded my-class">Button 1</CoolButton>
-<CoolButton isSmall isSuccess>Button 2</CoolButton> */}
+        {(this.props.isDanger && this.props.isSmall) && 
+        <button className={`button is-danger is-small ${this.props.className}`}>{this.props.children}</button>
+        }
+        {(this.props.isSuccess && this.props.isSmall) && 
+        <button className={`button is-success is-small`}>{this.props.children}</button>
+        }
+        </div>
+        );
+      }
+    }
+    export default CoolButton;
