bb4d7382b537d700b574d01e2e00baf391a3448a
mario salvatierra <msalv006@fiu.edu>
2019-07-22 20:59:51 -0400

    added message

--- starter-code/src/components/main/Main.js
+++ starter-code/src/components/main/Main.js
@@ -4,6 +4,7 @@ import 'bulma/css/bulma.css';
 import Navbar from "../navbar/Navbar";
 import Formfield from "../formfield/Formfield";
 import Coolbutton from "../coolbutton/Coolbutton";
+import Message from "../message/Message";
 
 
 class Main extends Component {
@@ -16,28 +17,33 @@ class Main extends Component {
         <Navbar></Navbar>
 
 
-        <div class="columns">
-  <div class="column">
+        <div className="columns">
+  <div className="column">
   
   </div>
-  <div class="column">
+  <div className="column">
       <Formfield label="Name" type="text" placeholder="e.g Alex Smith"></Formfield>
       <Formfield label="Email" type="email" placeholder="e.g. alexsmith@gmail.com"></Formfield>
       <Formfield label="Password" type="password" placeholder="password"></Formfield>
       
 <div className="level">
 
-  <div className="level-item" Style="margin-top:20px;">
+  <div className="level-item" >
 
       <Coolbutton isSuccess className="button is-danger">Button 2</Coolbutton>
   </div>
 
 </div>
+<div className="container">
 
+    <Message isInfo title="Hello World">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>.
+    </Message>
+</div>
 
     
   </div>
-  <div class="column">
+  <div className="column">
   
   </div>
   
