ea81c470dde747a31018c9145b89198f29fcb1e9
Marco Accardi <contact.marco.accardi@gmail.com>
2019-09-23 20:20:28 +0200

    missing bonus 5

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -6,50 +6,10 @@ import CoolButton from "./CoolButton";
 import Signup from "./Signup";
 
 const App = () => {
-  const name = {
-    type: "text",
-    label: "Name",
-    placeholder: "Alex Smith"
-  };
-
-  const email = {
-    type: "email",
-    label: "Email",
-    placeholder: "alexsmith@gmail.com"
-  };
-
-  const password = {
-    type: "password",
-    label: "Password",
-    placeholder: "password"
-  };
-
-  const button1 = {
-    type: "",
-    name: "Button 1"
-  };
-
-  const button2 = {
-    type: "",
-    name: "Button 2"
-  };
+  
   return (
     <Fragment>
-      <Navbar />
-      <FormField data={name} />
-      <FormField data={email} />
-      <CoolButton
-        isSmall
-        isDanger
-        className="is-rounded my-class"
-        name={button1.name}
-      />
-      <CoolButton isSmall isSuccess name={button2.name} />
-      <Signup>
-        <FormField data={name} />
-        <FormField data={email} />
-        <FormField data={password} />
-      </Signup>
+      <Signup />
     </Fragment>
   );
 };
--- starter-code/src/Signup.js
+++ starter-code/src/Signup.js
@@ -1,18 +1,57 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Navbar from "./Navbar";
+import CoolButton from "./CoolButton";
+import FormField from "./FormField";
 import "bulma/css/bulma.css";
 
-const Signup = props => {
+const Signup = () => {
+  const name = {
+    type: "text",
+    label: "Name",
+    placeholder: "Alex Smith"
+  };
+
+  const email = {
+    type: "email",
+    label: "Email",
+    placeholder: "alexsmith@gmail.com"
+  };
+
+  const password = {
+    type: "password",
+    label: "Password",
+    placeholder: "password"
+  };
+
+  const button1 = {
+    name: "Button 1"
+  };
+
+  const button2 = {
+    name: "Button 2"
+  };
+
+  const submit = {
+    name: "Submit",
+    type: "submit"
+  };
   return (
-    <div className="field">
-      <label className="label">{props.data.label}</label>
-      <div className="control">
-        <input
-          className="input"
-          type={props.data.type}
-          placeholder={props.data.placeholder}
-        />
-      </div>
-    </div>
+    <Fragment>
+      <Navbar />
+      <form>
+        <FormField data={name} />
+        <FormField data={email} />
+        <FormField data={password} />
+        <CoolButton name={submit.name} type={submit.type} />
+      </form>
+      <CoolButton
+        isSmall
+        isDanger
+        className="is-rounded my-class"
+        name={button1.name}
+      />
+      <CoolButton isSmall isSuccess name={button2.name} />
+    </Fragment>
   );
 };
 
