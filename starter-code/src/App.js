97364a5631a08e44e000d510f011c4368ec5f4bb
“Natalia <natgarea@gmail.com>
2019-09-30 19:21:44 +0200

    adds navbar

---
+++ starter-code/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import 'bulma/css/bulma.css';
+import Navbar from "./Navbar"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar></Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
