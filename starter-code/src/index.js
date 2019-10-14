174de53625664b55b05c50dcd7c15749f95c5483
Anedom <marco.modena@tutanota.com>
2019-10-14 15:55:20 -0300

    iteration 1

---
+++ starter-code/src/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import 'bulma/css/bulma.css';
+
+import App from './App';
+import Navbar from './Navbar';
+
+ReactDOM.render(
+    <Navbar />,
+    document.getElementById('root')
+)
