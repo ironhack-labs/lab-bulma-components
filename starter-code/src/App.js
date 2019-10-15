0aec373a3d57c3f2dab945e560a511898ed6992a
Mônica Damasceno <monica.damasceno@gitlab.com>
2019-10-14 21:44:23 -0300

    Iteration 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
-import Navbar from './components/navbar.js'
+import React, { Component, Fragment } from "react";
+import "./App.css";
+import Navbar from "./components/navbar.js";
+import FormField from "./components/FormField.js";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-      </div>
+      <Fragment>
+        <div className="App">
+          <Navbar />
+          <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+          <FormField
+            label="Email"
+            type="email"
+            placeholder="e.g. alexsmith@gmail.com"
+          />
+        </div>
+      </Fragment>
     );
   }
 }
---
+++ starter-code/src/components/FormField.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+const FormField = ({ label, type, placeholder }) => {
+  return (
+    <div class="field">
+      <label class="label">{label}</label>
+      <div class="control">
+        <input class="input" type={type} placeholder={placeholder} />
+      </div>
+    </div>
+  );
+};
+
+export default FormField;
