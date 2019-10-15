fe5c0fe59036ad90dc8e35efa873b2e84efd2825
Mônica Damasceno <monica.damasceno@gitlab.com>
2019-10-14 21:28:46 -0300

    Iteration 1

---
+++ starter-code/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/navbar.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default App;
