a2bdf5d56a6a8c2ecc5d9c583794bce2ebaa4d5a
pradeepa24 <sh.pradeepa01@gmail.com>
2019-11-11 11:39:42 -0500

    merging

---
+++ starter-code/src/components/nav-bar-component/nav-bar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class NavBar extends React.Component {
+    render() {
+        return (
+            <nav className="navbar is-fixed-top"></nav>
+        )
+    }
+}
+export default NavBar;
