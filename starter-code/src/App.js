766274fcb76c2a382118c7bb0e838ba18eb14416
maerd19 <revolutions_333@hotmail.com>
2019-11-10 18:22:57 -0600

    third iteration

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,14 +1,56 @@
 import React, { Component } from 'react';
 import Navbar from './Components/Navbar';
 import FormField from './Components/FormField';
+import CoolButton from './Components/CoolButton';
 
 class App extends Component {
+    constructor(props) {
+        super();
+        this.modes = {
+            isActive: "is-active",
+            isBlack: "is-black",
+            isCentered: "is-centered",
+            isDanger: "is-danger",
+            isDark: "is-dark",
+            isFocused: "is-focused",
+            isGrouped: "is-grouped",
+            isHovered: "is-hovered",
+            isInfo: "is-info",
+            isInverted: "is-inverted",
+            isLarge: "is-large",
+            isLight: "is-light",
+            isLink: "is-link",
+            isLoading: "is-loading",
+            isMedium: "is-medium",
+            isOutlined: "is-outlined",
+            isPrimary: "is-primary",
+            isRight: "is-right",
+            isRounded: "is-rounded",
+            isSelected: "is-selected",
+            isSmall: "is-small",
+            isStatic: "is-static",
+            isSuccess: "is-success",
+            isText: "is-text",
+            isWarning: "is-warning",
+            isWhite: "is-white"
+        };
+    }
+
     render() {
         return (
             <div className='App'>
                 <Navbar />
-                <FormField label='Name' type='text' placeholder='e.g Alex Smith'/>
-                <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+                <div className='Form'>
+                    <FormField label='Name' type='text' placeholder='e.g Alex Smith'/>
+                    <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+                </div>
+                
+                <div className='Buttons'>
+                    {/* <CoolButton isSmall isDanger className="is-rounded my-class">Button 1</CoolButton>
+                    <CoolButton isSmall isSuccess>Button 2</CoolButton> */}
+                    {Object.values(this.modes).map((x, i) => <CoolButton button={x}>Button {i}</CoolButton>)}
+
+                </div>
             </div>
         )
     }
---
+++ starter-code/src/Components/CoolButton.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class CoolButton extends Component {
+    // render() {
+    //     let strClass = 'button is-small';
+    //     strClass += this.props.isDanger ? ' is-rounded my-class is-danger' : '';
+    //     strClass += this.props.isSuccess ? ' is-success' : '';
+
+    //     return <button className={strClass}>{this.props.children}</button>                    
+    // }
+    render() {
+        console.log(this.props);
+        return (
+            <div>
+                <button className={`button ${this.props.button}`}>{this.props.children}</button>
+            </div>
+        )
+    }
+}
+
+export default CoolButton;
