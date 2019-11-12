2f8d5a73499d8f9a4e6aff909b45f439dfd57ad3
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-11 20:50:03 -0500

    delete starter code

--- bulma-practice/src/components/CoolButtons.js
+++ bulma-practice/src/components/CoolButtons.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 class CoolButton extends Component {
   render() {
     return (
-        <>
+      <>
         <a className={this.props.className}>{this.props.children}</a>
         {/* <a className="button is-rounded my-class is-danger is-small">Button1</a>
         <a className="button is-small is-success">Button2</a> */}
