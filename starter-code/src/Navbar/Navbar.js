592c4e328efdd282d709521475a0ed391ca3eea5
Flor Urbinati <flor29600@hotmail.com>
2019-12-02 17:54:18 +0100

    navbar

---
+++ starter-code/src/Navbar/Navbar.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className='navbar is-light'>
+        <div className='navbar-brand'>
+          <a className='navbar-item' href='https://bulma.io'>
+            <img src='https://bulma.io/images/bulma-logo.png' alt='Bulma: a modern CSS framework based on Flexbox' width='112' height='28' />
+          </a>
+          <div className='navbar-burger burger' data-target='navbarExampleTransparentExample'>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+
+        <div id='navbarExampleTransparentExample' className='navbar-menu'>
+          <div className='navbar-start'>
+            <a className='navbar-item' href='#'>
+              Home
+            </a>
+          </div>
+
+          <div className='navbar-end'>
+            <div className='navbar-item'>
+              <div className='field is-grouped'>
+                <p className='control'>
+                  <a className="button is-info" 
+                    target='_blank'
+                    href='#'>
+                    <span>Login</span>
+                  </a>
+                </p>
+                <p className='control'>
+                  <a className='button is-primary' href='#'>
+                    <span>Signup</span>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
