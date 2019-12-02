e69b35550bd64fdb9feff0773ccf36016fdaa10f
Flor Urbinati <flor29600@hotmail.com>
2019-12-02 19:10:50 +0100

    signup

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from "react";
-import Navbar from "./Navbar/Navbar";
-import FormField from "./FormField/FormField";
-import CoolButton from "./Button/CoolButton";
+import Signup from "./Signup/Signup";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar></Navbar>
-        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
-<FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
-<CoolButton className="is-small is-danger is-rounded my-class button">Button 1</CoolButton>
-<CoolButton className= "is-small is-success button">Button 2</CoolButton>
+        <Signup></Signup>
       </div>
     );
   }
---
+++ starter-code/src/Signup/Signup.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Navbar from "../Navbar/Navbar";
+import FormField from "../FormField/FormField";
+import CoolButton from "../Button/CoolButton";
+
+export default class Signup extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar></Navbar>
+        <FormField label='Name' type='text' placeholder='e.g Alex Smith' />
+        <FormField label='Email' type='email' placeholder='e.g. alexsmith@gmail.com' />
+        <FormField label='Password' type='password' placeholder='password' />
+        <CoolButton className='is-small is-link button'>Submit</CoolButton>
+      </div>
+    );
+  }
+}
