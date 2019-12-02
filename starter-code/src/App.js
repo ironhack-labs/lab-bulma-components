c3390193f372a5e7e2cb55d76406126b98d64b40
Rafael Bosch <rbllado@outlook.com>
2019-12-02 18:01:10 +0100

    last version lab

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -7,12 +7,14 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <SignUp />
-        <br/>
-        <Message classMessage="message is-info" title="Hello World">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          <strong>Pellentesque risus mi</strong>.
-        </Message>
+        <div className="container">
+          <SignUp />
+          <br />
+          <Message classMessage="message is-info" title="Hello World">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            <strong>Pellentesque risus mi</strong>.
+          </Message>
+        </div>
       </div>
     );
   }
