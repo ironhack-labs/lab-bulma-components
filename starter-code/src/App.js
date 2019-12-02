87ec4fee700f271f6be6518375e7aeabc8638adf
Flor Urbinati <flor29600@hotmail.com>
2019-12-02 18:15:42 +0100

    input fields

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import Navbar from "./Navbar/Navbar";
+import FormField from "./FormField/FormField";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar></Navbar>
+        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+<FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+
       </div>
     );
   }
