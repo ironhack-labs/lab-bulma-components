aa8806162cc3e8ce888f566872b5a54c0aad3673
USUARIO <usuario@Macs-MacBook-Pro.local>
2019-12-03 08:51:05 +0100

    iniciando las iteraciones, no carga el navegador

---
+++ starter-code/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+
+function App() {
+
+    return (
+        <NavBar/>
+    )
+}
+
+export default App;
---
+++ starter-code/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import App from "./App";
+import ReactDOM from "react-dom";
+import "bulma/css/bulma.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
