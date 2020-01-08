27fc7a44c2dcd49b98264bf70d1db29c0d1e2c68
Jeferson Silva <jeferson.s.silva1@gmail.com>
2020-01-08 20:50:27 -0300

    Feito a nav bar

---
+++ starter-code/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Nav from "./Nav";
+import 'bulma/css/bulma.css';
+
+ReactDOM.render(
+  //<App />,
+  <Nav />,
+  document.getElementById('root')
+);
