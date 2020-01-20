95a54c1c44df9011333a38033e4c3216e919f0e2
lpolon <lpolon@gmail.com>
2020-01-20 18:03:01 -0300

    first iteration done

---
+++ starter-code/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import 'bulma/css/bulma.css'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <Navbar />
+  )
+}
+
+export default App;
