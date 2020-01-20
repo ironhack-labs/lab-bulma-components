b365a21b8fce04131bd72ee9aebcc9908cc9514c
Giu Roperto <giu.roperto@gmail.com>
2020-01-20 19:34:03 -0300

    up until iteration 3

---
+++ starter-code/src/components/CoolButton.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class CoolButton extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    let allClasses = 'button';
+    if (this.props.isSmall) {
+      allClasses += ' is-small';
+    }
+    if (this.props.isDanger) {
+      allClasses += ' is-danger';
+    }
+    if (this.props.isSuccess) {
+      allClasses += ' is-success';
+    }
+    if (this.props.className) {
+      allClasses += ` ${this.props.className}`;
+    }
+    return (
+      <button className={allClasses}>{this.props.children}</button>
+    )
+  }
+}
+
+export default CoolButton;
---
+++ starter-code/src/components/FormField.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class FormField extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className="field">
+  <label className="label">{this.props.label}</label>
+  <div className="control">
+    <input className="input" type={this.props.type} placeholder={this.props.placeholder} />
+  </div>
+</div>
+    )
+  }
+}
+
+export default FormField;
