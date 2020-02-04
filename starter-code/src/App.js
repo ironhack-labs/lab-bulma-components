7676aa9a643429a706fbe9711f238f22339c28de
samo <audreychevron@gmail.com>
2020-02-04 13:52:40 +0100

    pas fini

---
+++ starter-code/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Navbar from "./Navbar";
+import FormField from "./FormField";
+
+
+class App extends Component {
+
+    render() {
+
+        return (
+            <div className="App">
+                <Navbar/>
+                <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+                <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+            </div>
+        )
+
+
+    }
+}
+
+export default App;
---
+++ starter-code/src/FormField.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class FormField extends Component {
+    render() {
+      return (
+            <div className="field">
+                <label className="label">Name</label>
+                <div className="control">
+                    <input className="input" type="text" placeholder="e.g Alex Smith" />
+                </div>
+            </div>
+            <div className="field">
+                <label className="label">Email</label>
+                <div className="control">
+                    <input className="input" type="email" placeholder="e.g. alexsmith@gmail.com" />
+                </div>
+            </div>
+        );
+    }
+}
+export default FormField;
