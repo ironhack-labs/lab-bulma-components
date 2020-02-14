4e546a1a357a51ead659842d21039de871812ff1
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-02-13 20:55:03 -0600

    Add navbar

--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import 'bulma/css/bulma.css';
 import App from "./App.js";
+import Navbar from "./components/navbar";
 
 ReactDOM.render(
-    <App />,
+    <div>
+        <Navbar/>
+        <App/>
+    </div>,
     document.getElementById("root")
 );
