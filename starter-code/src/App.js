0b1109d05d26ea5585dd905bde3b8c97b622cc74
Gustavo Cruz <gustavocruzb@outlook.com>
2020-02-14 11:53:40 -0600

    iteration 2 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,9 +1,14 @@
-import React  from 'react';
-import Navigation from './Navigation'
+import React, { Fragment }  from 'react';
+import Navigation from './Navigation';
+import FormField from './FormField';
 
 const App = props => {
     return (
+        <Fragment>
         <Navigation />
+        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+        </Fragment>
     )
 }
 
