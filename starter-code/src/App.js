0455ba294fd1c39683c04106b771f147bbb8c8ad
Servo MacPro <tarzanboycraft@hotmail.com>
2020-02-13 21:25:09 -0600

    container

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,29 @@
 import Navbar from './components/Navbar.js';
 import FormField from './components/FormField.js';
+import Container from './components/Container.js'
 import CoolButton from './components/CoolButton.js';
 import React from 'react';
 
 
 function App() {
     return (
-      <div>
-          <Navbar/>
-          <div className="columns is-centered">
-              <div className="column is-6">
-              <FormField name="Name" type="text" placeholder="e.g Alex Smith"></FormField>
-              <FormField name="Email" type="email" placeholder="alexsmith@gmail.com"></FormField>
-              <FormField name="Password" type="password" placeholder="******"></FormField>
-              </div>
-          </div>
-          <div className="columns">
-              <div className="column has-text-centered">
-              <CoolButton isLarge isPrimary isRounded>Submit</CoolButton>
-              </div>
-          </div>
-      </div>
+        <div>
+            <Navbar />
+            <Container>
+                <div className="columns is-centered">
+                    <div className="column is-6">
+                        <FormField name="Name" type="text" placeholder="e.g Alex Smith"></FormField>
+                        <FormField name="Email" type="email" placeholder="alexsmith@gmail.com"></FormField>
+                        <FormField name="Password" type="password" placeholder="******"></FormField>
+                    </div>
+                </div>
+                <div className="columns">
+                    <div className="column has-text-centered">
+                        <CoolButton isLarge isPrimary isRounded>Submit</CoolButton>
+                    </div>
+                </div>
+            </Container>
+        </div>
     );
-  }
-  export default App;
+}
+export default App;
