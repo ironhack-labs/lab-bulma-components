59fa2966e259653fef4d8c50074818b586d5390a
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-02-14 16:47:30 -0600

    Some cleanup

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -6,28 +6,6 @@ import "./App.css";
 
 class App extends Component {
     render() {
-        const formatName = (user) => {
-            return `${user.firstName} ${user.lastName}`;
-        };
-    
-        const user = {
-            firstName: "Juan",
-            lastName: "Perez",
-            avatarUrl: ""
-        };
-
-        const displayAvatar = (user) => {
-            if (user.avatarUrl) {
-                return <img src={user.avatarUrl} />
-            } else {
-                return <img src='https://s3.amazonaws.com/owler-image/logo/ironhack_owler_20180828_221413_original.png' width='300' height='300' />
-            }
-        }
-    
-        const element = (
-            <h2>Hello, {formatName(user)}!</h2>
-        );
-
         return (
             <div className="App">
                 <Navbar/>
