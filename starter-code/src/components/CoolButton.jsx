7adcc594ba58d0572ec1903860da65ac9cec0a60
Tonalli <tonalli@xanum.uam.mx>
2020-02-14 14:09:46 -0600

    Update button

--- starter-code/src/components/CoolButton.jsx
+++ starter-code/src/components/CoolButton.jsx
@@ -1,17 +1,49 @@
 import React from "react";
 
 const CoolButton = (props) => {
-    const clases = {
-        isSmall: "is-small",
-        isDanger: "is-danger",
-        isSuccess: "is-success"
-    };
+    const allCases = {
+        isActive: 'is-active',
+        isBlack: 'is-black',
+        isCentered: 'is-centered',
+        isDanger: 'is-danger',
+        isDark: 'is-dark',
+        isFocused: 'is-focused',
+        isGrouped: 'is-grouped',
+        isHovered: 'is-hovered',
+        isInfo: 'is-info',
+        isInverted: 'is-inverted',
+        isLarge: 'is-large',
+        isLight: 'is-light',
+        isLink: 'is-link',
+        isLoading: 'is-loading',
+        isMedium: 'is-medium',
+        isOutlined: 'is-outlined',
+        isPrimary: 'is-primary',
+        isRight: 'is-right',
+        isRounded: 'is-rounded',
+        isSelected: 'is-selected',
+        isSmall: 'is-small',
+        isStatic: 'is-static',
+        isSuccess: 'is-success',
+        isText: 'is-text',
+        isWarning: 'is-warning',
+        isWhite: 'is-white',
+      };
 
     const formatClass = (props) => {
-        return `button`;
+        let result = "button";
+
+        for (let key of Object.keys(props)) {
+            if (key !== "children") {
+                result += ` ${allCases[key]}`;
+            }
+        }
+        
+        return result;
     };
+
     return (
-        <button className={formatClass(props)}>Button 1</button>
+        <button className={formatClass(props)}>{props.children}</button>
     );
 };
 
