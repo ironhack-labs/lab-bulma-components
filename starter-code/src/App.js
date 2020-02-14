eca9754346948940a28bdb4dcf50cb1bed92cd16
Tonalli <tonalli@xanum.uam.mx>
2020-02-14 14:46:09 -0600

    Update App.js

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -33,6 +33,7 @@ class App extends Component {
                 <Navbar/>
                 <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
                 <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+                <CoolButton isSmall isDanger className="is-rounded my-class">Button 1</CoolButton>
                 <CoolButton isSmall isSuccess>Button 2</CoolButton>
             </div>
         );
