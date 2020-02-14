d4243a3c24dded97bad6d615520a68088bd43b4d
Tonalli <tonalli@xanum.uam.mx>
2020-02-14 13:35:26 -0600

    Fix FormField

--- starter-code/src/components/FormField.jsx
+++ starter-code/src/components/FormField.jsx
@@ -4,8 +4,8 @@ const FormField = (props) => {
     return (
         <div className="field">
             <label className="label">{props.label}</label>
-            <div class="control">
-                <input class="input" type={props.type} placeholder={props.placeholder} />
+            <div className="control">
+                <input className="input" type={props.type} placeholder={props.placeholder} />
             </div>
         </div>
     );
