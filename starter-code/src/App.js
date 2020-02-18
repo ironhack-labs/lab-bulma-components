68096c32cd4bfabd5ab85c77f1a9746c34ef1a7d
Daedra <foglzerika@gmail.com>
2020-02-18 11:41:22 -0600

    Adding forminput component

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from './components/Navbar'
-
+import FormField from './components/FormField'
 class App extends Component {
 
     render() {
       return (
         <div className="App">
             <Navbar />
+            <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+            <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com"/>
         </div>
       );
     }
