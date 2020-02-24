2f43504459d77cc0acdafded86660cf07ab2def8
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-02-24 19:15:59 +0100

    bulma menu and basic setup done

---
+++ starter-code/src/App.js
@@ -0,0 +1,15 @@
+// import "./App.css";
+
+import React, {Component} from 'react'
+import Navbar from "./Navbar.js";
+import 'bulma/css/bulma.css';
+
+function App() {
+  return (
+    <div>
+     <Navbar />      
+    </div>
+  )
+}
+
+export default App;
