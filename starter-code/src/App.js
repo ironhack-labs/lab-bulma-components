87e2f623cd09a20781fc1694f18ec6f8f4ef13f1
Jaime Hidalgo Fernandez <jaimehidalgofernandez@gmail.com>
2020-02-24 19:53:39 +0100

    done

---
+++ starter-code/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+// import './App.css';
+import Signup from './sign-up'
+
+function App() {
+  return (
+      <section>
+      
+<Signup></Signup>
+
+      </section>
+  );
+}
+
+export default App;
