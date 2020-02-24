12463d2237867de2775986722995c17ce9af706e
FrankyMolina <fjmolinatorrejon@gmail.com>
2020-02-24 23:51:42 +0100

    mm...

---
+++ starter-code/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import 'bulma/css/bulma.css';
+import "./App.css";
+import Navbar from "./Navbar/Navbar"
+//import FormField from "./Formfield/Formfield"
+
+
+class App extends Component {
+  render() {
+    return ( 
+      <div className="App">
+        <Navbar></Navbar>
+        <NameForm label="Name" type="text" placeholder="e.g Alex Smith" /> 
+        {/* <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" /> */}
+        <h1> Hello Ironhackers! </h1>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ starter-code/src/form/NameFrom.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class NameForm extends React.Component() {
+
+    render(){
+
+        return {
+
+            
+        }
+    }
+}
+
+
+export default NameFrom;
