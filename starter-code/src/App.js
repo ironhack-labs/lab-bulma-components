50716babd33cff184744df34f4544fafb87774c6
Stefan Berk <stefan@k2ff.de#>
2020-02-24 18:36:29 +0100

    with formfield

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Nav from "./Nav.js"
+import Nav from "./Nav.js";
+import FormField from "./FormField.js";
 
 export default class App extends React.Component {
     render() {
         return (
             <div >
                 <Nav />
-                hello
+                <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+                <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
             </div>
         );
     }
