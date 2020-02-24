0232043ae9c6097ace5f3f2cc68190587cedbe36
Stefan Berk <stefan@k2ff.de#>
2020-02-24 18:32:41 +0100

    wit hanvbar

---
+++ starter-code/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Nav.js"
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div >
+                <Nav />
+                hello
+            </div>
+        );
+    }
+}
