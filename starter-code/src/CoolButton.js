bb8b2f1e51e360187420008b10e8f7772ec2582b
annkay108 <annkhachhibhoya@gmail.com>
2020-02-25 08:01:44 +0100

    done

---
+++ starter-code/src/CoolButton.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CoolButton extends Component {
+  render() {
+    return (
+      <div>
+        <button className={this.props.className} >{this.props.children}</button>
+      </div>
+    );
+  }
+}
