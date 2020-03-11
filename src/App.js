c15c7284e0558952f31fd0506edbb109aaaa4e78
Kevin Medina <kmedinakm@hotmail.com>
2020-03-10 21:52:58 -0400

    iteration 3 CoolButton component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
-import Navbar from "./navbar/Navbar";
-import FormField from "./formfield/FormField";
+import Navbar from "./components/navbar/Navbar";
+import FormField from "./components/formfield/FormField";
+import CoolButton from "./components/coolButton/CoolButton";
 
-const App = () => {
+const App = (props) => {
   return (
     <>
       <Navbar />
       <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
-      {/* <FormField
-        label="Email"
-        type="email"
-        placeholder="e.g. alexsmith@gmail.com"
-      /> */}
+      <CoolButton isSmall isDanger className="is-rounded my-class">
+        Button 1
+      </CoolButton>
     </>
   );
 };
