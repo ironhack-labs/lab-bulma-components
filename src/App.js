65b8113c5fad3f79307083c3bab7e75489bf13d9
Kevin Medina <kmedinakm@hotmail.com>
2020-03-10 21:09:02 -0400

    iteration 2 FormField component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import Navbar from './navbar/Navbar';
+import React from "react";
+import Navbar from "./navbar/Navbar";
+import FormField from "./formfield/FormField";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      {/* <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      /> */}
+    </>
+  );
 };
 
 export default App;
