950711797407239b78412e2b2fe495ad6427e104
Veronica Donoso <veronicadonoso1506@gmail.com>
2020-03-11 13:06:19 -0400

    Create navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Navbar from './navbar/Navbar'
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <>
+  {/* <h1>Bulma</h1> */}
+  <div>
+    <Navbar />
+  </div>
+  </>
+  )
 };
 
 export default App;
---
+++ src/navbar/Navbar.js
@@ -0,0 +1,18 @@
+import 'bulma/css/bulma.css';
+import React from 'react'
+
+
+
+const Navbar = () => 
+<nav className="navbar is transparent">
+    <div className="navbar-brand">
+        <a className="nabvar-item" href="https://bulma.io">
+         <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28"></img>   
+        </a>
+        <a className="navbar-item" href="https//bulma.io/">Signup</a>
+        <a className="navbar-item">Login</a>
+    </div>
+</nav>;
+
+
+export default Navbar
