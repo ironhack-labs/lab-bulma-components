fada5d7138b1a7bc7bdca6af829c99a6090c4016
Michaela DeCamara <michaela.decamara@gmail.com>
2020-03-13 11:18:13 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import NavBar from './navbar/Navbar';
+import FormField from './formfield/FormField';
+import CoolButton from './coolbutton/CoolButton';
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <>
+      <NavBar />
+      <FormField label='Name' type='text' placeholder='e.g Alex Smith' />
+      <FormField label='Email' type='email' placeholder='e.g. alexsmith@gmail.com' />
+      <CoolButton isSmall isDanger className='is-rounded my-class'>
+        Button 1
+      </CoolButton>
+      <CoolButton isSmall isSuccess>
+        Button 2
+      </CoolButton>
+    </>
+  );
 };
 
 export default App;
