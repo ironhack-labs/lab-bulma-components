a8a9533010e12baccdbfe0863aef7cc2cf5492a0
Pau Larrea <paularrea12@hotmail.com>
2020-03-30 19:49:40 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Signup from "./Signup"
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return  <div>
+          <Signup/>
+          </div> 
+
 };
 
 export default App;
