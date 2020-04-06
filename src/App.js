bd133ba56720eb79da47430cb77844ff8f02bf68
Javier Varela <jvarela@strands.com>
2020-04-07 00:56:48 +0200

    FormField

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import Navbar from './navbar/Navbar'
+import FormField from './formField/FormField'
 
 const App = () => {
-  return <Navbar/>;
+  return (
+    <div>
+      <Navbar/>
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith"/>
+      <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com"/>
+    </div>
+  );
 };
 
 export default App;
