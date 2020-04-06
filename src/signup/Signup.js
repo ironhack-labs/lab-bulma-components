0f58bd9574b0ac04fd787f3b988260991e4c743d
Javier Varela <jvarela@strands.com>
2020-04-07 01:52:43 +0200

    signup

---
+++ src/signup/Signup.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'bulma/css/bulma.css';
+import CoolButton from '../coolButton/CoolButton'
+import Navbar from '../navbar/Navbar'
+import FormField from '../formField/FormField'
+
+const Signup = () => {
+    return (
+        <div>
+            <Navbar/>
+            <FormField label="Name" type="text" placeholder="e.g Alex Smith"/>
+            <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com"/>
+            <FormField label="Password" type="password" placeholder="*****"/>
+            <CoolButton className="button is-success" href="" value="Submit"/>
+        </div>
+    );
+};
+    
+export default Signup;
