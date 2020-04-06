b0e529ef1d5e5ede9e6ef98f090ef304af88dfff
Pablolo <pablo_olondriz@hotmail.com>
2020-04-06 20:25:03 +0200

    setting up and iteration 1 done

---
+++ lab-bulma/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './navbar/Navbar'
+import 'bulma/css/bulma.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <header className=""> 
+        <Navbar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
