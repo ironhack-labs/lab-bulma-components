83748ac8105872b085f7c40cb876b01a99be785c
Carles Galí Bou <carles_ga@hotmail.com>
2020-04-06 11:10:03 +0200

    Work environment done and first Iteration done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Navbar from './navbar/Navbar';
+import 'bulma/css/bulma.css';
+import './navbar/Navbar.css';
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <Navbar />
+  )
 };
 
 export default App;
---
+++ src/navbar/Navbar.js
@@ -0,0 +1,45 @@
+import 'bulma/css/bulma.css';
+import React, { Component } from 'react';
+
+
+class Navbar extends Component {
+	render() {
+		return (
+			<nav class="navbar" role="navigation" aria-label="main navigation">
+ 				<div class="navbar-brand">
+   					<a class="navbar-item" href="https://bulma.io">
+      				<img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"></img>
+    				</a>
+    				<a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      				<span aria-hidden="true"></span>
+      				<span aria-hidden="true"></span>
+      				<span aria-hidden="true"></span>
+    				</a>
+  			</div>
+
+				<div id="navbarBasicExample" class="navbar-menu">
+					<div class="navbar-start">
+						<a class="navbar-item">
+							Home
+						</a>
+					</div>
+
+					<div class="navbar-end">
+						<div class="navbar-item">
+							<div class="buttons">
+								<a class="button is-info">
+									<strong>Log in</strong>
+								</a>
+								<a class="button is-primary">
+									Sign up
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</nav>
+		)
+	}
+}
+
+export default Navbar
