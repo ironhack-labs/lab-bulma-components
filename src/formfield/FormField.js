f45b3860753d7d1c0af56d52ba85860fc4e8dff1
Michael Mueller <michael@miniguide.co>
2020-04-08 20:56:02 +0200

    Iteration 1

---
+++ src/formfield/FormField.js
@@ -0,0 +1,11 @@
+// components/FormField.js
+
+import React from 'react';
+import './FormField.css';
+import 'bulma/css/bulma.css';
+
+function FormField() {
+
+}
+
+export default FormField;
---
+++ src/navbar/Navbar.js
@@ -0,0 +1,48 @@
+// components/Navbar.js
+
+import React from 'react';
+import 'bulma/css/bulma.css';
+
+function Navbar() {
+  return (
+    <nav class="navbar is-light">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
+        </a>
+        <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
+      <div id="navbarExampleTransparentExample" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item" href="https://bulma.io/">
+            Home
+          </a>
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="field is-grouped">
+              <p class="control">
+                <a class="button is-info" href="">
+                  <span>Login</span>
+                </a>
+              </p>
+              <p class="control">
+                <a class="button is-primary" href="">
+                  <span>Signup</span>
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
