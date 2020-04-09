1cef63fc9aafcad628adeb50d2a251fcd6d4ba26
gerardss95 <gerardsolesanz@hotmail.com>
2020-04-09 17:25:43 +0200

    iteration 3 done

---
+++ src/coolbutton/CoolButton.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import 'bulma/css/bulma.css';
+
+const CoolButton = (props) => {
+    return (
+        <a className={props.className}  href={props.href}>
+            <span>{props.value}</span>
+        </a>
+    );
+};
+
+export default CoolButton; 
--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bulma/css/bulma.css';
 import './Navbar.css';
+import CoolButton from '../coolbutton/CoolButton';
 
 function navbar () {
   return (
@@ -27,16 +28,10 @@ function navbar () {
           <div class="navbar-item">
             <div class="field is-grouped">
               <p class="control">
-                <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="https://bulma.io" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms">
-                  <span>
-                    Login
-                  </span>
-                </a>
+              <CoolButton className="button is-success" href="" value="Login"/>
               </p>
               <p class="control">
-                <a class="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.8.0/bulma-0.8.0.zip">
-                  <span>Signup</span>
-                </a>
+              <CoolButton className="button is-primary" href="" value="Signup"/>
               </p>
             </div>
           </div>
