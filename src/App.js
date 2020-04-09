2111369819b132e97cc120ba36c19a79a66354c6
gerardss95 <gerardsolesanz@hotmail.com>
2020-04-09 17:18:20 +0200

    íteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './navbar/Navbar'
+import FormField from './formfield/FormField'
 import 'bulma/css/bulma.css';
 import './App.css';
 
@@ -8,6 +9,8 @@ function App() {
     <div className="">
       <header className=""> 
         <Navbar />
+        <FormField label="Name" type="text" placeholder="e.g Alex Smith"/>
+        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com"/>
       </header>
     </div>
   );
