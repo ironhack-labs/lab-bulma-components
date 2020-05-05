4d5340476aac528c439ba97147b735cd3f4a1455
Arturo Moretón Arrillaga <arturomoretonarrillaga@MacBook-Air-de-Arturo.local>
2020-05-05 09:19:34 +0200

    hasta it 3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+
 import React from 'react';
+import Navbar from './Navbar/Navbar';
+import FormField from './FormField/FormField';
+//import CoolButton from './Coolbutton/Coolbutton';
+import 'bulma/css/bulma.css';
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <div>
+    <Navbar />
+    <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+    <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+    </div>
+  
+  )
 };
 
 export default App;
