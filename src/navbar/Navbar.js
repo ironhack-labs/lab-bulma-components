0cb08885406787e50ada66fc7bba46ad7b7a2c37
Francesco Saccone <francescosaccone95@live.it>
2020-05-26 21:19:10 +0200

    Francesco WEB_DEV_PARTTIME_BERLIN

---
+++ src/navbar/Navbar.js
@@ -0,0 +1,52 @@
+import React from "react";
+import 'bulma/css/bulma.css';
+
+function Navbar()
+{
+    return(
+        <div>
+<nav className="navbar is-transparent">
+  <div className="navbar-brand">
+    <a className="navbar-item" href="https://bulma.io">
+      <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
+    </a>
+    <div className="navbar-burger burger" data-target="navbarExampleTransparentExample">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+
+  <div id="navbarExampleTransparentExample" className="navbar-menu">
+    <div className="navbar-start">
+      <a className="navbar-item" href="https://bulma.io/">
+        Home
+      </a>
+    </div>
+
+    <div className="navbar-end">
+      <div className="navbar-item">
+        <div className="field is-grouped">
+          <p className="control">
+            <a className="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="https://bulma.io" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms">
+              <span>
+                Log in
+              </span>
+            </a>
+          </p>
+          <p className="control">
+            <a className="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.8.2/bulma-0.8.2.zip">
+              <span>Sign up</span>
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+
+        </div>
+    )
+}
+
+export default Navbar
