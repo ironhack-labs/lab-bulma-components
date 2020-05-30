669bce40b32ee58ff6ddb8643704ecdad7e953d1
Stefano <oddouu@gmail.com>
2020-05-30 14:04:59 +0100

    iteration 1 and 2 done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./navbar/Navbar";
+import FormField from "./formfield/FormField";
+import "bulma/css/bulma.css";
+import { render } from "react-dom";
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <div>
+      <Navbar />
+      <form>
+        <FormField
+          label="Name"
+          type="text"
+          placeholder="e.g Alex Jonathan Smith"
+        />
+        <FormField
+          label="Email"
+          type="email"
+          placeholder="e.g. alexsmith@gmail.com"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default App;
