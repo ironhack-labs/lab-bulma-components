c754cf569f1708d3ae305c65b2502d8e84008f5c
Marc De Mena <mdemena@gmail.com>
2020-07-08 18:01:12 +0200

    Iteration 5 Finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navbar from './navbar/Navbar';
-import FormField from './formfield/FormField';
 
 const App = () => {
 	return (
@@ -8,14 +7,6 @@ const App = () => {
 			<header>
 				<Navbar />
 			</header>
-			<main>
-				<FormField label="Name" type="text" placeholder="e.g Alex Smith" />
-				<FormField
-					label="Email"
-					type="email"
-					placeholder="e.g. alexsmith@gmail.com"
-				/>
-			</main>
 		</div>
 	);
 };
