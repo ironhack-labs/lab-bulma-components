96930c195073ce677e3f08939c845ea886088842
David de Lamo <d.lamo.valverde@gmail.com>
2020-07-09 20:16:41 +0200

    iteration 5 done

--- src/signup/Signup.js
+++ src/signup/Signup.js
@@ -4,6 +4,7 @@ import Navbar from '../navbar/Navbar'
 import FormField from '../formfield/FormField'
 import CoolButton from '../coolbutton/CoolButton'
 import Container from '../container/Container'
+import Message from '../message/Message';
 
 const Signup = () => {
     return (
@@ -14,6 +15,9 @@ const Signup = () => {
                 <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
                 <FormField label="Password" type="password" placeholder="Type your password" />
                 <CoolButton isSuccess isRounded>Submit</CoolButton>
+                <Message isInfo title='Hello World'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>.
+                </Message>
             </Container>
         </div>
     )
