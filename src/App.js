f6c18689349be7eee9dd2d343788ae9e13173d7b
Marcela Vilas Boas <marcela.vilasboas@gmail.com>
2020-07-09 10:10:52 +0200

    iterations 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import Navbar from './navbar/Navbar';
+import FormField from './formfield/FormField';
+
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <div>
+      <Navbar />
+      <div className="formDiv">
+        <FormField 
+          label="Name"
+          type="text"
+          placeholder="e.g Alex Smith"
+        />
+        <FormField 
+          label="Email"
+          type="email"
+          placeholder="e.g alexsmith@gmail.com"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default App;
