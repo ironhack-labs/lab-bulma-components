a9ca727fe076de253b3a81cae5195efbbd877fbc
Robert Ledezma <Robert@MacBook-Pro-de-Robert-2.local>
2020-07-11 01:24:08 +0200

    Iteración 2 done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from './navbar/Navbar';
+import FormField from './formfield/FormField'
 
 
 const App = () => {
   return (
     <div>
       <Navbar />
+
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
      
     </div>
   );
--- src/formfield/Formfield.js
+++ src/formfield/Formfield.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 
-export default class Formfield extends Component {
+import 'bulma/css/bulma.css';
+
+
+class FormField extends Component {
     render() {
         return (
-            <div>
-                
+            <div className='field'>
+            <label className='label'>{this.props.label}</label>
+            <div className='control'>
+                <input className='input' type={this.props.type} placeholder={this.props.placeholder} />
             </div>
+        </div>
         )
     }
 }
+
+export default FormField
