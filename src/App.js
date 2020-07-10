0db23937d3316a74397caddbd2e8c0443c6c97eb
Alberto Correia <alberto_cpv@hotmail.com>
2020-07-10 18:24:54 +0200

    iteration 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from 'react'
 import Navbar from "./Navbar/Navbar"
+import FormField from './formfield/FormField'
 
 
 const App = () => {
   return (
     <div>
      <Navbar/>
+     <FormField label="Name" type="text" placeholder="e.g Alex Smith"/>
+     <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com"/>
+   
     </div>
   )
 };
 
-export default App;
+export default App
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -5,42 +5,48 @@ class Navbar extends Component {
     render() {
         return (
             <div>
-            <nav classname="navbar is-transparent">
-            <div classname="navbar-brand">
-              <a classname="navbar-item" href="https://bulma.io">
-                <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28"/>
-              </a>
-              
-            </div>
-          
-            <div id="navbarExampleTransparentExample" classname="navbar-menu">
-              <div classname="navbar-start">
-                <a classname="navbar-item" href="/">
-                  Home
-                </a>
-               
-              </div>
-          
-              <div classname="navbar-end">
-                <div classname="navbar-item">
-                  <div classname="field is-grouped">
-                    <p classname="control">
-                      <a classname="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="https://bulma.io" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms">
-                        <span>
-                          Log in
-                        </span>
-                      </a>
-                    </p>
-                    <p classname="control">
-                      <a classname="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.9.0/bulma-0.9.0.zip">
-                        <span>Signup</span>
-                      </a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </nav>
+              <nav class="navbar is-transparent">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28"/>
+    </a>
+    <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+
+  <div id="navbarExampleTransparentExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item" href="https://bulma.io/">
+        Home
+      </a>
+     
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="field is-grouped">
+          <p class="control">
+            <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="https://bulma.io" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms">
+           
+              <span>
+                Login 
+              </span>
+            </a>
+          </p>
+          <p class="control">
+            <a class="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.9.0/bulma-0.9.0.zip">
+             
+              <span>Signup</span>
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
           </div>
         )
     }
