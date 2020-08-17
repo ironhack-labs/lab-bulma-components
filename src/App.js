9f70c2f7286c3d51f90c6de52cb39e1ac928323a
MartaLourido <martalouri@gmail.com>
2020-08-18 01:48:04 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import 'bulma/css/bulma.css'
+import Navbar from "./navbar/Navbar";
+import FormField from './formfield/FormField.js'
+import CoolButton from "./buttons/CoolButton";
+import Signup from './signup/Signup.js'
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return(
+    <div class = "myApp">
+      <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+      <CoolButton isSmall isDark className="is-rounded my-class">Button 1</CoolButton>
+      <CoolButton isSmall isSuccess>Button 2</CoolButton>
+      <Signup />
+    </div>
+  )
+
 };
 
 export default App;
