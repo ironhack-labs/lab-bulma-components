7dedbf79c42b1b30219a1a13d5196eacd0a59550
Alex Anthopoulos <zephoxcodese@gmail.com>
2020-09-08 16:56:29 +0200

    done

---
+++ src/signup/Signup.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from '../navbar/Navbar';
+import FormField from '../formfield/FormField';
+import CoolButton from '../button/CoolButton';
+
+
+const Signup = props => {
+
+  return (
+  	<div>
+    <Navbar />
+    <FormField label="Name" type="text" placeholder="your name" />
+	<FormField label="Email" type="email" placeholder="your email" />
+	<FormField label="Password" type="password" placeholder="*******" />
+	<CoolButton buttonText='SUBMIT' />
+	</div>
+  );
+};
+
+export default Signup
