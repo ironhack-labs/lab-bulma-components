eaa5c3b3a6abe7ae004851374b3814670015a1f7
alejanglez <64974849+alejanglez@users.noreply.github.com>
2020-09-19 14:42:48 +0200

    IT 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "./navbar/Navbar.js";
+import FormField from "./formfield/FormField";
 
 const App = () => {
   return (
-    <div className="Navbar">
+    <div className="container">
       <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Superman" />
+      <FormField label="Email" type="email" placeholder="e.g. your email" />
+      {/* <CoolButton isPrimary buttonText="AYYY" />
+      <Signup /> */}
     </div>
   );
 };
---
+++ src/formfield/FormField.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const FormField = (props) => {
+  return (
+    <div className="field">
+      <label className="label">{props.label}</label>
+      <div className="control">
+        <input
+          className="input"
+          type={props.type}
+          placeholder={props.placeholder}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FormField;
