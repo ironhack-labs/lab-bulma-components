f1c9e153b13ea6d815817d1a81518b9344fffae4
alejanglez <64974849+alejanglez@users.noreply.github.com>
2020-09-19 13:55:23 +0200

    IT 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./navbar/Navbar.js";
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <div className="Navbar">
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
