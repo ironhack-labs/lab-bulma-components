cd352334853c29295f653870b2da891a714dfb53
Sunitha Mannam <mannam.sunitha@gmail.com>
2020-09-20 14:07:45 +0200

    complete with bonus

--- src/Message/Message.js
+++ src/Message/Message.js
@@ -3,8 +3,11 @@ import "bulma/css/bulma.css";
 import { getClassName } from "../utils/utils.js";
 
 const Message = (props) => {
+  let resClass = "message  ";
+  resClass += getClassName(props);
+
   return (
-    <article className="message is-info">
+    <article className={resClass}>
       <div className="message-header">
         <p>{props.title}</p>
         <button className="delete" aria-label="delete"></button>
--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -3,29 +3,32 @@ import Navbar from "../Navbar/Navbar.js";
 import FormField from "../FormField/FormField.js";
 import CoolButton from "../CoolButton/CoolButton.js";
 import Message from "../Message/Message.js";
+import Container from "../Container/Container.js";
 
 const Signup = () => {
   return (
     <div>
-      <Navbar />
+      <Container>
+        <Navbar />
 
-      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
-      <FormField
-        label="Email"
-        type="email"
-        placeholder="e.g. alexsmith@gmail.com"
-      />
-      <CoolButton
-        isSmall
-        isDanger
-        className="is-rounded my-class"
-        btnText="Sign Up"
-      />
+        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+        <FormField
+          label="Email"
+          type="email"
+          placeholder="e.g. alexsmith@gmail.com"
+        />
+        <CoolButton
+          isSmall
+          isDanger
+          className="is-rounded my-class"
+          btnText="Sign Up"
+        />
 
-      <Message isInfo title="Hello World">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
-        <strong>Pellentesque risus mi</strong>.
-      </Message>
+        <Message isInfo title="Hello World">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+          <strong>Pellentesque risus mi</strong>.
+        </Message>
+      </Container>
     </div>
   );
 };
