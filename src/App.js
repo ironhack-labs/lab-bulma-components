578b6d0bfa459fc5a76e114ee6b08e5f4fe5853d
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-09-21 22:39:33 -0400

    working on iteration 3, class names for buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Navbar from "./navbar/Navbar";
+import FormField from "./formfield/Formfield";
+import CoolButton from "./cool-button/CoolButton";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+      <CoolButton isSmall isDanger className="button is-rounded my-class">Button 1</CoolButton>
+      <CoolButton isSmall isSuccess>Button 2</CoolButton>
     </div>
     );
 };
---
+++ src/cool-button/CoolButton.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import 'bulma/css/bulma.css';
+
+const buttonClassses = {
+    isActive: 'is-active',
+    isBlack: 'is-black',
+    isCentered: 'is-centered',
+    isDanger: 'is-danger',
+    isDark: 'is-dark',
+    isFocused: 'is-focused',
+    isGrouped: 'is-grouped',
+    isHovered: 'is-hovered',
+    isInfo: 'is-info',
+    isInverted: 'is-inverted',
+    isLarge: 'is-large',
+    isLight: 'is-light',
+    isLink: 'is-link',
+    isLoading: 'is-loading',
+    isMedium: 'is-medium',
+    isOutlined: 'is-outlined',
+    isPrimary: 'is-primary',
+    isRight: 'is-right',
+    isRounded: 'is-rounded',
+    isSelected: 'is-selected',
+    isSmall: 'is-small',
+    isStatic: 'is-static',
+    isSuccess: 'is-success',
+    isText: 'is-text',
+    isWarning: 'is-warning',
+    isWhite: 'is-white',
+};
+
+const CoolButton = (props) => {
+    console.log(props);
+    return (
+        <button className={props.className}>{props.children}</button>
+    );
+};
+
+export default CoolButton;
