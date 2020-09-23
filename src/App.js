dc42a27f898fa35528d367e3406b282db5c875ff
ariannavalle <61655861+ariannavalle@users.noreply.github.com>
2020-09-22 21:46:24 -0400

    Complete iteration 1, 2, and 3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Navbar from './navbar/Navbar';
+import FormField from './formfield/FormField'
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return <div>
+    <Navbar/>
+    <FormField label='Name' placeholder='e.g Alex Smith'/>
+    <FormField label='Email' placeholder='e.g. alexsmith@gmail.com'/>
+  </div>;
 };
 
 export default App;
