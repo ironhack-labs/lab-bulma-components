ac659289767e8762835ec10c31e7ef477f35b052
Arua Monteiro <aruamonteiro@gmail.com>
2020-09-29 21:11:20 -0300

    Done

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "bulma/css/bulma.css";
+import Signup from './Signup'
+
+const App = () => {
+  return <Signup/>;
+};
+
+export default App;
---
+++ src/components/CoolButton.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const CoolButton = (props) => {
+  const types = {
+    "is-primary": "is-primary",
+    "is-success": "is-success",
+    "is-danger": "is-danger",
+  };
+
+  const className = props.className || '';
+  const typesKeys = Object.keys(types);
+  const propsKeys = Object.keys(props);
+  const propClass = typesKeys.filter((el) => propsKeys.includes(el)).join(" ");
+  const classes = `button ${className} ${propClass}`;
+
+  return <button className={classes}>{props.text}</button>;
+};
+
+export default CoolButton;
