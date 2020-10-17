f2812f0e5fa2659fb10c1c3d3f383bd0306a822c
anna dodo <anna.dorenskaya@gmail.com>
2020-10-17 11:51:59 +0200

    1st iteration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+//import './Navbar/node_modules/bulma/css/bulma.css';
+import Navbar from './Navbar/Navbar'
+
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (  
+    <div>
+      <h1>Bulma</h1>
+        <Navbar />
+    </div>
+  )
 };
 
 export default App;
