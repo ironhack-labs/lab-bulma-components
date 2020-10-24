3064b190b2b7497e2cbb2fd813962b8ab33a25c3
unimatrix2 <dnny.nis@gmail.com>
2020-10-24 17:08:00 -0300

    iteration 4: render signup component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import 'bulma/css/bulma.css'
-import Navbar from './navbar/Navbar';
+/* import Navbar from './navbar/Navbar';
+import FormField from './formfield/FormField';
+import CoolButton from './coolbutton/CoolButton'; */
+import Signup from './singup/Signup';
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <Signup />
   )
 };
 
