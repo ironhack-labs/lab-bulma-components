11d6cc7702d5dc4ca2135e4358848abd0761714c
unimatrix2 <dnny.nis@gmail.com>
2020-10-27 20:13:03 -0300

    custom bonus: implemented state to close message

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -2,14 +2,28 @@ import React from 'react';
 import returnClassNames from '../../utils/classManager';
 import './Message.css';
 
-const Message = (props) => 
-    <article className={`message ${returnClassNames(props)} custom-message`}>
-        <div className="message-header">
-            <p>{props.title}</p>
-            <button class="delete" aria-label="delete"></button>
-        </div>
-        <div className="message-body">{props.children}</div>
-    </article>
 
+class Message extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { showMessage: true }
+    }
+
+    handleCloseclick = () => this.setState({ showMessage: this.state.showMessage = false });
+
+    render() {
+        if (this.state.showMessage) {
+            return (
+                <article className={`message ${returnClassNames(this.props)} custom-message`}>
+                <div className="message-header">
+                    <p>{this.props.title}</p>
+                    <button className="delete" aria-label="delete" onClick={this.handleCloseclick}></button>
+                </div>
+                <div className="message-body">{this.props.children}</div>
+                </article>
+            )
+        } else return <article className={`message ${returnClassNames(this.props)} closeMessage`}></article>;
+    }
+}
 
 export default Message;
