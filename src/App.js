88e8c909c1adbd7a2b37051b978408a2c6f62de5
isisabreu <isisrcabreu@gmail.com>
2020-10-27 21:23:11 -0300

    done until interaction 3(partilally)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import 'bulma/css/bulma.css';
+
+import './App.css';
+
+import Navbar from './components/Navbar/Navbar';
+
+import Formfield from './components/Formfield/Formfield';
+
+import CoolButton from './components/CoolButton/CoolButton';
+
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <div>
+      <Navbar />
+      <Formfield label="Name" type="text" placeholder="e.g Alex Smith" />
+      <Formfield label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+
+      <CoolButton>Button 1</CoolButton>
+    </div>
+  );
 };
 
 export default App;
