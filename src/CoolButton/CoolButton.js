6c2fed2ee235be8d6dd2c9aaa8612f065e321449
gtretow <guilherme.tretow@gmail.com>
2020-11-10 19:41:16 -0300

    iteration 2 and 3 done

---
+++ src/CoolButton/CoolButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "bulma/css/bulma.css";
+
+function CoolButton() {
+  return (
+    <div>
+      <button className="button is-rounded my-class is-danger is-small">
+        Button 1
+      </button>
+      <button className="button is-small is-success">Button 2</button>
+    </div>
+  );
+}
+
+export default CoolButton
