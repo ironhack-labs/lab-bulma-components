1b3167a701155fb9cb837323293cc737d0680725
Fellype <fellype.ads@gmail.com>
2020-11-11 14:21:04 -0300

    container 40% width

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -2,7 +2,11 @@ import React from "react";
 import "bulma/css/bulma.css";
 
 function Container(props) {
-  return <div className="container" style={{}}>{props.children}</div>;
+  return (
+    <div className="container" style={{ width: "40%" }}>
+      {props.children}
+    </div>
+  );
 }
 
 export default Container;
