f53a2af1ffbdcd262e53c494dc1d3f252b938776
denisonamaral <denison.amaral@outlook.com>
2020-11-11 09:53:31 -0300

    upload exercise lab-bulma

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from "react";
+import "bulma/css/bulma.css";
+import Navbar from "./navbar/Navbar";
+import FormField from "./formfield/FormField";
+import CoolButton from "./coolbutton/coolButton";
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <div>
+      <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
+      <CoolButton className="button is-rounded my-class is-danger is-small">
+        Button 1
+      </CoolButton>
+      <CoolButton className="button is-small is-success">Button 2</CoolButton>
+    </div>
+  );
 };
 
 export default App;
