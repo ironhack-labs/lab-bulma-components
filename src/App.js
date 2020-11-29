3301e333ac5761da851a57b1d89cf0c781ccaac0
Abbie <abbie@summers.gg>
2020-11-29 22:33:49 +0100

    Iteration 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
 import Navbar from "./navbar/Navbar";
+import FormField from "./formfield/formField";
+import CoolButton from "./coolbutton/coolButton"
+import "bulma/css/bulma.css";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
+      <CoolButton isSmall isDanger className="is-rounded my-class">Button 1</CoolButton>
+
+    </div>
+  );
 };
 
 export default App;
--- src/coolbutton/coolButton.js
+++ src/coolbutton/coolButton.js
@@ -2,11 +2,14 @@ import React from "react";
 import "bulma/css/bulma.css";
 
 function CoolButton(props) {
-  return (
-    <div>
-      <button className={`button{props.className}>{props.children}`}</button>
-    </div>
-  );
-}
+        return (
+          <div>
+            <button className="button is-rounded my-class is-danger is-small">
+              Button 1
+            </button>
+            <button className="button is-small is-success">Button 2</button>
+          </div>
+        );
+      }
 
 export default CoolButton;
