0c2738a35dc4e3d10eacb03b65d89db67d5f29c2
RaquelRB <raquelrobles95@gmail.com>
2020-11-30 17:54:31 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import 'bulma/css/bulma.css';
+// import Navbar from './navbar/Navbar'
+// import FormField from './formfield/FormField'
+import Signup from './signup/Signup';
+
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <div className="App">
+      {/* <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" /> */}
+      <Signup/>
+    </div>
+  )
 };
 
 export default App;
---
+++ src/navbar/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import 'bulma/css/bulma.css';
+import CoolButton from '../coolbutton/CoolButton'
+
+const Navbar = ()=>{
+    return (
+        <div className="Navbar">
+                    <nav class="navbar is-transparent">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="https://bulma.io">
+                <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28"/>
+                </a>
+                <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+                <span></span>
+                <span></span>
+                <span></span>
+                </div>
+            </div>
+            <div id="navbarExampleTransparentExample" class="navbar-menu">
+                <div class="navbar-start">
+                <a class="navbar-item" href="https://bulma.io/"> Home </a>
+                </div>
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                    <CoolButton className="button is-danger" type="submit" title="Log in"/>
+                    <CoolButton className="button is-success" type="submit" title="Sign Up"/>
+                    </div>
+                </div>
+             </div>
+            </nav>
+        </div>
+    )
+}
+export default Navbar;
