e8d130e62d6d21531f4d0de7deff553335045927
Juan Miguel Sansinenea <juanmi.sansinenea@gmail.com>
2020-11-30 19:26:26 +0100

    navbar customized to taste

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -33,12 +33,12 @@ class Navbar extends React.Component {
                 Home
               </a>
               <div className="navbar-item has-dropdown is-hoverable">
-                <a
+                {/* <a
                   className="navbar-link"
                   href="https://bulma.io/documentation/overview/start/"
                 >
                   Docs
-                </a>
+                </a> */}
                 <div className="navbar-dropdown is-boxed">
                   <a
                     className="navbar-item"
@@ -92,17 +92,17 @@ class Navbar extends React.Component {
                 <div className="field is-grouped">
                   <p className="control">
                     <a
-                      className="bd-tw-button button"
+                      className="button is-link"
                       data-social-network="Twitter"
                       data-social-action="tweet"
                       data-social-target="https://bulma.io"
                       target="_blank"
                       href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms"
                     >
-                      <span className="icon">
+                      {/* <span className="icon">
                         <i className="fab fa-twitter"></i>
-                      </span>
-                      <span>Tweet</span>
+                      </span> */}
+                      <span>Login</span>
                     </a>
                   </p>
                   <p className="control">
@@ -110,10 +110,10 @@ class Navbar extends React.Component {
                       className="button is-primary"
                       href="https://github.com/jgthms/bulma/releases/download/0.9.1/bulma-0.9.1.zip"
                     >
-                      <span className="icon">
+                      {/* <span className="icon">
                         <i className="fas fa-download"></i>
-                      </span>
-                      <span>Download</span>
+                      </span> */}
+                      <span>Signup</span>
                     </a>
                   </p>
                 </div>
