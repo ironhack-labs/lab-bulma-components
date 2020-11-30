0e0ab0f22ec7e29cd2507f54c686fb0dcb488494
ararita <munjizavesna@gmail.com>
2020-11-30 17:13:10 +0100

    Iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Fragment } from "react";
 import "bulma/css/bulma.css";
 import Navbar from "./navbar/Navbar";
+import FormField from "./formfield/FormField";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <Fragment>
+      {" "}
+      <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
+      <FormField label="Password" type="password" placeholder="123456789" />{" "}
+    </Fragment>
+  );
 };
 
 export default App;
