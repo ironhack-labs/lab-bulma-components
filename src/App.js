2c2412776c2dd0944d35095ae9bdddaf623293a0
ararita <munjizavesna@gmail.com>
2020-11-30 18:15:48 +0100

    Iteration 4 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React, { Fragment } from "react";
-import "bulma/css/bulma.css";
-import Navbar from "./navbar/Navbar";
-import FormField from "./formfield/FormField";
-import CoolButton from "./coolbutton/CoolButton";
+
+import Signup from "./Signup";
 
 const App = () => {
-  return (
-    <Fragment>
-      {" "}
-      <Navbar />
-      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
-      <FormField
-        label="Email"
-        type="email"
-        placeholder="e.g. alexsmith@gmail.com"
-      />
-      <FormField label="Password" type="password" placeholder="123456789" />{" "}
-    </Fragment>
-  );
+  return <Signup />;
 };
 
 export default App;
---
+++ src/Signup.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "bulma/css/bulma.css";
+import Navbar from "./navbar/Navbar";
+import FormField from "./formfield/FormField";
+import CoolButton from "./coolbutton/CoolButton";
+
+const Signup = () => {
+  return (
+    <div>
+      <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
+      <FormField label="Password" type="password" placeholder="123456789" />
+    </div>
+  );
+};
+
+export default Signup;
