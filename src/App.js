6965ce5c173e8f8d74534db5a3e6beb907a4b519
Carlos García <61150045+carlos-garcia-dev@users.noreply.github.com>
2020-12-01 00:29:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './App.css';
 
-//Components
-// import Navbar from './navbar/Navbar'
-// import FormField from './formfield/FormField'
+Components
+import Navbar from './navbar/Navbar'
+import FormField from './formfield/FormField'
 
 import SignUp from './components/signup/SignUp'
 import Container from './components/container/Container'
@@ -12,14 +12,14 @@ const App = () => {
   return (
     <main>
       
-    {/* <header><Navbar></Navbar></header>    
+    <header><Navbar></Navbar></header>    
 
     <section>
         <form class="is-centered">
           <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
           <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
         </form>       
-    </section> */}
+    </section>
 
     <section class="frame">
       <SignUp></SignUp>
