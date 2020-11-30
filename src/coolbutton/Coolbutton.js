7934a83ae16fd8a4ac7a20d0824249966b3fcde4
Arturo Moretón Arrillaga <arturomoretonarrillaga@Air-de-Arturo.home>
2020-11-30 23:09:32 +0100

    hasta aqui he llegado

---
+++ src/coolbutton/Coolbutton.js
@@ -0,0 +1,15 @@
+import "bulma/css/bulma.css";
+
+import React from "react";
+
+const CoolButton = () => {
+    return (
+        <div>
+            <button className="button is-rounded my-class is-danger is-small">Button 1</button>
+          <button className="button is-small is-success">Button 2</button>
+        </div>
+
+    );
+}
+
+export default CoolButton; 
--- src/signup/Signup.js
+++ src/signup/Signup.js
@@ -1,22 +1,22 @@
-import React from 'react';
 import "bulma/css/bulma.css";
+
+import React from "react";
 
 import Navbar from "./navbar/Navbar";
 import FormField from "./formfield/FormField";
 import CoolButton from "./coolbutton/CoolButton";
 
-const App = () => {
-
-  return (
-    <div>
-      <Navbar />
+const Signup = () => {
+    return (
+      <div>
+          <Navbar />
       <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
     <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
-    <CoolButton isSmall isDanger className="is-rounded my-class">Button 1</CoolButton>
-    <CoolButton isSmall isSuccess>Button 2</CoolButton>
-    </div>
+    <FormField label="Password" type="password" placeholder="enter your password" />
+    <CoolButton isSmall isDanger className="is-rounded my-class">Submit</CoolButton>
+          </div>
 
-  )
-};
 
-export default App;
+    );}
+
+    export default Signup;
