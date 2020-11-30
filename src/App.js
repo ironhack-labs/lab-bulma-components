1cf7dbf335c825e8ce4a420f4c8a15e5090bba93
Roma Dorado <doradoroma@gmail.com>
2020-11-30 20:38:42 +0100

    [WEB-MAD-DE]José Dorado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Navbar from './navbar/Navbar';
+import 'bulma/css/bulma.css';
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return <h1>Bulma</h1>
 };
+
+function App() {
+  return (
+    <main>
+      <Navbar/>
+    </main>
+  )
+}
 
 export default App;
---
+++ src/navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import 'bulma/css/bulma.css'
+
+const Navbar = () => {
+    return (
+        <div className="Navbar">
+            <nav class="navbar is-transparent">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="https://bulma.io">
+                        <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28"/>
+                    </a>
+                    <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                <div id="navbarExampleTransparentExample" class="navbar-menu">
+                    <div class="navbar-start">
+                        <a class="navbar-item" href="https://bulma.io/">
+                            Home
+                        </a>
+                    </div>
+                    <div class="navbar-end">
+                        <div class="navbar-item">
+                            <div class="field is-grouped">
+                                <p class="control">
+
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar 
