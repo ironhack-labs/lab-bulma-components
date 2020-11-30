2a1f794c35ec5407d58680a3d117e2157850253b
Juan Miguel Sansinenea <juanmi.sansinenea@gmail.com>
2020-11-30 19:08:49 +0100

    bulma menu imported successfully

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./navbar/Navbar";
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
