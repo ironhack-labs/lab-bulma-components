913516876f229a2cc142c8c8c87d6033925da51c
manuelbarreda <manubarreda69@gmail.com>
2020-11-30 18:57:11 +0100

    Prueba

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
             <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
             <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" /> 
             <CoolButton className="button is-rounded my-class is-small is-hovered" name="Submit"></CoolButton>
+            <Signup/>
             <Message isInfo title='Hello World'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>.
             </Message>
-            <Signup/>
           </main>
       )
 }
