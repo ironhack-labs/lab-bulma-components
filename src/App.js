9fe93a4becc1b46c8aa65913d9c7f510667654fd
Ferran Martinez <ferran.ms8@gmail.com>
2020-11-30 21:22:42 +0100

    done mandatory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import 'bulma/css/bulma.css';
+import Navbar from './navbar/Navbar'
+import FormField from './formfield/Formfield'
+import CoolButton from './components/CoolButton'
 
 const App = () => {
   return <h1>Bulma</h1>;
 };
+
+class App extends React.Component {
+  render() {
+    return (
+      <div id="App">
+        <Navbar />
+        <FormField label="Email" input="" placeholder="Email"/>
+      </div>
+    )
+  }
+}
 
 export default App;
