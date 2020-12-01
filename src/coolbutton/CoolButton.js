bc36bad191f1c017146a47a83033d544078b3dc4
Marc Gotzens <marcgotzens@hotmail.com>
2020-12-01 15:24:25 +0100

    finished iteration 3 with some fixes

--- src/coolbutton/CoolButton.js
+++ src/coolbutton/CoolButton.js
@@ -2,40 +2,15 @@ import "bulma/css/bulma.css";
 
 import React from "react";
 
-const names = {
-  isActive: "is-active",
-  isBlack: "is-black",
-  isCentered: "is-centered",
-  isDanger: "is-danger",
-  isDark: "is-dark",
-  isFocused: "is-focused",
-  isGrouped: "is-grouped",
-  isHovered: "is-hovered",
-  isInfo: "is-info",
-  isInverted: "is-inverted",
-  isLarge: "is-large",
-  isLight: "is-light",
-  isLink: "is-link",
-  isLoading: "is-loading",
-  isMedium: "is-medium",
-  isOutlined: "is-outlined",
-  isPrimary: "is-primary",
-  isRight: "is-right",
-  isRounded: "is-rounded",
-  isSelected: "is-selected",
-  isSmall: "is-small",
-  isStatic: "is-static",
-  isSuccess: "is-success",
-  isText: "is-text",
-  isWarning: "is-warning",
-  isWhite: "is-white",
-};
+import names from "../ruleSet.js";
 
 export default function CoolButton(props) {
-  let classes = "button ";
+  let classes = "button " + props.className + " ";
   for (let key in props) {
     console.log("keys", key);
-    if (key in names) {
+    console.log("names[key]", names[key]);
+
+    if (names[key]) {
       classes += names[key] + " ";
       console.log("Classes", classes);
     }
---
+++ src/signup/Signup.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Navbar from "./navbar/Navbar";
+import FormField from "./formfield/FormField";
+import CoolButton from "./coolbutton/CoolButton";
+
+export default function Signup() {
+  return <div></div>;
+}
