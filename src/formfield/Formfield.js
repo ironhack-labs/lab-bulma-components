1dff01f42574c8f935529e203782352cc161b5a0
Roma Dorado <doradoroma@gmail.com>
2020-12-01 09:11:06 +0100

    Le he intentado dar algo de fantasia mientras desayuno

--- src/formfield/Formfield.js
+++ src/formfield/Formfield.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import 'bulma/css/bulma.css'
+import './Formfield.css'
 
 const Formfield = props => {
 
     return (
-        <form>
+        <form class="form1">
             <div className='field'>
                 <label className='label'>{props.label}</label>
-                    <div class="control">
+                    <div class="control1">
                         <input class="input" type={props.type} placeholder={props.placeholder} />
                     </div>
             </div>
