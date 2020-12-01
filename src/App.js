df1c3b34355795bc3b2b1582dd45a4e2cf74dd44
msastreharo <msastreharo@gmail.com>
2020-12-01 16:54:30 +0100

    Buttons working but without style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
+
+import Navbar from "./Navbar/Navbar";
+import FormField from "./Formfield/Formfield";
+import CoolButton from "./CoolButton/CoolButton";
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <div className="App">
+      <Navbar />
+      <FormField label="Email" type="email" placeholder="email@email.com" />
+      <CoolButton isSmall isSuccess>
+        Accept
+      </CoolButton>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/CoolButton/CoolButton.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "bulma/css/bulma.css";
+
+const ruleSet = {
+  isActive: "is-active",
+  isBlack: "is-black",
+  isCentered: "is-centered",
+  isDanger: "is-danger",
+  isDark: "is-dark",
+  isFocused: "is-focused",
+  isGrouped: "is-grouped",
+  isHovered: "is-hovered",
+  isInfo: "is-info",
+  isInverted: "is-inverted",
+  isLarge: "is-large",
+  isLight: "is-light",
+  isLink: "is-link",
+  isLoading: "is-loading",
+  isMedium: "is-medium",
+  isOutlined: "is-outlined",
+  isPrimary: "is-primary",
+  isRight: "is-right",
+  isRounded: "is-rounded",
+  isSelected: "is-selected",
+  isSmall: "is-small",
+  isStatic: "is-static",
+  isSuccess: "is-success",
+  isText: "is-text",
+  isWarning: "is-warning",
+  isWhite: "is-white",
+};
+
+function CoolButton(props) {
+  let startingStr = `button ${props.className}`;
+  for (let key in props) {
+    if (ruleSet[key]) {
+      startingStr += `${ruleSet[key]}`;
+    }
+  }
+  return <button className={startingStr}>{props.children}</button>;
+}
+
+export default CoolButton;
