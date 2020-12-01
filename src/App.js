c40029d9a0ff23242982128bb3560d38632f52a3
GosiaMas <malgorzata.maslowska81@gmail.com>
2020-12-01 17:59:11 +0000

    including iteration 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
+import Formfield from "./formfield/Formfield";
+import Navbar from "./Navbar/Navbar";
+import 'bulma/css/bulma.css';
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+  <div className="App">
+  <Navbar />
+  <Formfield 
+  label="Name" 
+  type="text" 
+  placeholder="e.g Alex Smith" />
+<Formfield 
+label="Email" 
+type="email" 
+placeholder="e.g. alexsmith@gmail.com" />
+  </div>
+  )
 };
 
 export default App;
