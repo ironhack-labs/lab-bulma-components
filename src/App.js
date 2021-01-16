96e7a674368b1e36cb268d80dc6d4c72c8b56b65
Dirk Kelderman <dirkkelderman@gmail.com>
2021-01-16 13:51:03 +0100

    iteration 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './navbar/Navbar';
+import FormField from './formfield/FormField'
 import 'bulma/css/bulma.css';
 
 
@@ -7,6 +8,8 @@ function App() {
   return(
     <div>
       <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
     </div>
   );
 }
