363b09bd19907691ed286a32557767664a481f8d
Driele An <drielean@gmail.com>
2021-02-03 22:27:18 -0300

    finalizado

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SignUp from './SignUp';
+
+const App = () => (
+  <div className='App'>
+    <SignUp />
+  </div>
+);
+
+export default App;
---
+++ src/components/CoolButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const CoolButton = props => {
+  console.log(props);
+
+  const { isSmall, isDanger, className, isSuccess, children } = props;
+
+  const classNameString = `
+    button
+    ${className}
+    ${isSmall ? 'is-small' : ''}
+    ${isDanger ? 'is-danger' : ''}
+    ${isSuccess ? 'is-success' : ''}
+  `;
+
+  return <button className={classNameString}>{children}</button>;
+};
+
+export default CoolButton;
