83c1c89b4493cf6680063b601d1f844d3a05e4c8
Barbara Diaz <barbarad91@gmail.com>
2021-02-22 16:50:21 +0100

    Create form inputs

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import "./App.css"
 import Navbar from "./Navbar"
+import FormField from "./FormField"
 
 function App() {
   return (
     <>
       <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g. AlexSmith" />
+      <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
     </>
   )
 }
---
+++ src/components/FormField.js
@@ -0,0 +1,15 @@
+import "bulma/css/bulma.css"
+import "./FormField.css"
+
+const FormField = ({ label, type, placeholder }) => (
+  <>
+    <div class="field">
+      <label class="label">{label}</label>
+      <div class="control">
+        <input class="input" type={type} placeholder={placeholder} />
+      </div>
+    </div>
+  </>
+)
+
+export default FormField
