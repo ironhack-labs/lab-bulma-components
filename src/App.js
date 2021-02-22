7b5e682a985169b61c8d039cef197ee8f2ee3dde
dimitrijd <dimitrijdugan@gmail.com>
2021-02-22 18:25:20 +0100

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
+import "bulma/css/bulma.css";
+import Navbar from "/src/navbar/navbar";
+import Formfield from "/src/formfield/Formfield";
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <div>
+      <Navbar />
+      <Formfield label="Name" type="text" placeholder="e.g Alex Smith" />
+      <Formfield
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
+      {/* <CoolButton isSmall isDanger className="is-rounded my-class">
+        Button 1
+      </CoolButton>
+      <CoolButton isSmall isSuccess>
+        Button 2
+      </CoolButton> */}
+    </div>
+  );
 };
 
 export default App;
---
+++ src/CoolBotton/CoolButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "bulma/css/bulma.css";
+
+const CoolBottun = (props) => {
+  return <button className="button">{props.button}</button>;
+};
+
+export default CoolBottun;
