65abb51e2c06b56ed91a05d2d7d7c5130849d628
Barbara Diaz <barbarad91@gmail.com>
2021-02-22 16:32:26 +0100

    Update navbar content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,90 +22,15 @@ const Navbar = () => (
         <a class="navbar-item" href="https://bulma.io/">
           Home
         </a>
-        <div class="navbar-item has-dropdown is-hoverable">
-          <a
-            class="navbar-link"
-            href="https://bulma.io/documentation/overview/start/"
-          >
-            Docs
-          </a>
-          <div class="navbar-dropdown is-boxed">
-            <a
-              class="navbar-item"
-              href="https://bulma.io/documentation/overview/start/"
-            >
-              Overview
-            </a>
-            <a
-              class="navbar-item"
-              href="https://bulma.io/documentation/overview/modifiers/"
-            >
-              Modifiers
-            </a>
-            <a
-              class="navbar-item"
-              href="https://bulma.io/documentation/columns/basics/"
-            >
-              Columns
-            </a>
-            <a
-              class="navbar-item"
-              href="https://bulma.io/documentation/layout/container/"
-            >
-              Layout
-            </a>
-            <a
-              class="navbar-item"
-              href="https://bulma.io/documentation/form/general/"
-            >
-              Form
-            </a>
-            <hr class="navbar-divider" />
-            <a
-              class="navbar-item"
-              href="https://bulma.io/documentation/elements/box/"
-            >
-              Elements
-            </a>
-            <a
-              class="navbar-item is-active"
-              href="https://bulma.io/documentation/components/breadcrumb/"
-            >
-              Components
-            </a>
-          </div>
-        </div>
       </div>
 
       <div class="navbar-end">
         <div class="navbar-item">
-          <div class="field is-grouped">
-            <p class="control">
-              <a
-                class="bd-tw-button button"
-                data-social-network="Twitter"
-                data-social-action="tweet"
-                data-social-target="https://bulma.io"
-                target="_blank"
-                href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms"
-              >
-                <span class="icon">
-                  <i class="fab fa-twitter"></i>
-                </span>
-                <span>Tweet</span>
-              </a>
-            </p>
-            <p class="control">
-              <a
-                class="button is-primary"
-                href="https://github.com/jgthms/bulma/releases/download/0.9.1/bulma-0.9.1.zip"
-              >
-                <span class="icon">
-                  <i class="fas fa-download"></i>
-                </span>
-                <span>Download</span>
-              </a>
-            </p>
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light">Log in</a>
           </div>
         </div>
       </div>
