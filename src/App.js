f51d865e99a07785a56138a036d56640ca2b1c48
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2021-05-01 13:37:14 -0300

    add iteration 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import 'bulma/css/bulma.css';
+import Navbar from './components/navbar/Navbar'
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return <Navbar/>;
 };
 
 export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar is-transparent">
+
+            <div className="navbar-brand">
+                <a className="navbar-item" href="https://bulma.io" >
+                    <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
+                </a>
+                <div class="navbar-burger" data-target="navbarExampleTransparentExample">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            
+            <div className="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item" href="https://bulma.io/">
+                        Home
+                    </a>
+                </div>
+                <div className="navbar-end">
+                    <div className="field is-grouped">
+                        <div className="control">
+                            <a class="button is-info" href="/login">
+                                Login
+                            </a>
+                        </div>
+                        <div className="control">
+                            <a class="button is-primary" href="/login">
+                                Sign up
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
