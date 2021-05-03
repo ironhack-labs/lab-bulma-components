bdef9eef7db62237dc038164f621924e8d9bc017
Silinde87 <paurodriguezmolina@gmail.com>
2021-05-03 23:53:25 +0200

    fix Message class

--- src/message/Message.js
+++ src/message/Message.js
@@ -2,10 +2,7 @@ import React from 'react';
 import 'bulma/css/bulma.css';
 
 const Message = (props) => {
-    let additionalClass = '';
     let text = '';
-    if(props.isInfo) additionalClass = 'is-info';
-    console.log(props.children)
     props.children.forEach(el => {
         if(typeof el !== 'object'){
             text += el;
@@ -16,12 +13,15 @@ const Message = (props) => {
     
     return (
         <>
-            <article class={`message ${additionalClass}`}>
+            <article class={`message ${props.isInfo ? "is-info" : ""}`}>
                 <div class="message-header">
                     <p>{props.title}</p>
                     <button class="delete" aria-label="delete"></button>
                 </div>
-                <div class="message-body" dangerouslySetInnerHTML={{__html: text}}></div>
+                <div class="message-body" dangerouslySetInnerHTML={{__html: text}}>
+                {/* <div class="message-body">
+                    {props.children} */}
+                </div>
             </article>
         </>
     )
