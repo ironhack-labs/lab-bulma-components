3110469041231caca281ff39554f734ab5b93024
Jesica Nugnez <jesica.nugnez@gmail.com>
2021-05-03 18:56:34 +0200

    Navbar and formfield components done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Navbar from './Navbar/Navbar';
+import FormField from './formfield/FormField'
 
 const App = () => {
   return (
-    <div>
+    <div className="mainDiv">
+    <div className="navbar-div">
       <Navbar/>
     </div>
+    
+    <div className="formField-div">
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith"/>
+      <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com"/>
+      </div>
+    </div>
+    
    );
 };
 
--- src/formfield/FormField.js
+++ src/formfield/FormField.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import 'bulma/css/bulma.css';
 
-function FormField() {
+function FormField({ name, email }) {
     return(
         <div>
             <div class="field">
-                <label class="label">Name</label>
+                <label class="label">{name}</label>
                 <div class="control">
                     <input class="input" type="text" placeholder="e.g Alex Smith" />
                 </div>
             </div>
             <div class="field">
-                <label class="label">Email</label>
+                <label class="label">{email}</label>
                 <div class="control">
                     <input class="input" type="email" placeholder="e.g. alexsmith@gmail.com" />
                 </div>
