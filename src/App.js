5140d6446948f8de9c080f3e6a179dfce5736eaf
Tigran Harutyunyan <tigrandeboris@outlook.com>
2021-05-03 23:08:10 +0200

    Remove unneeded imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import 'bulma/css/bulma.css';
-import Navbar from './navbar/Navbar';
-import FormField from './formfield/FormField';
-import CoolButton from './coolbutton/CoolButton';
 import SignUp from './signup/SignUp';
 
 class App extends React.Component {
