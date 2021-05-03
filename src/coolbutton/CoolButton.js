b480e914674ad0a62fbb3a503deea5b4cbcb03d5
Marcia Faria <mfndnc@gmail.com>
2021-05-03 17:59:20 +0200

    Iteration 3 Bonus

--- src/coolbutton/CoolButton.js
+++ src/coolbutton/CoolButton.js
@@ -10,13 +10,43 @@ function dashToCamelCase(myStr) {
   });
 }
 
+const coolButtonClasses = {
+  isActive: 'is-active',
+  isBlack: 'is-black',
+  isCentered: 'is-centered',
+  isDanger: 'is-danger',
+  isDark: 'is-dark',
+  isFocused: 'is-focused',
+  isGrouped: 'is-grouped',
+  isHovered: 'is-hovered',
+  isInfo: 'is-info',
+  isInverted: 'is-inverted',
+  isLarge: 'is-large',
+  isLight: 'is-light',
+  isLink: 'is-link',
+  isLoading: 'is-loading',
+  isMedium: 'is-medium',
+  isOutlined: 'is-outlined',
+  isPrimary: 'is-primary',
+  isRight: 'is-right',
+  isRounded: 'is-rounded',
+  isSelected: 'is-selected',
+  isSmall: 'is-small',
+  isStatic: 'is-static',
+  isSuccess: 'is-success',
+  isText: 'is-text',
+  isWarning: 'is-warning',
+  isWhite: 'is-white',
+};
+
 const App = (props) => {
   //console.log(props);
 
   let addedClass = Object.keys(props).reduce((acc, cur) => {
     if (cur == 'className') return acc + ' ' + props[cur];
-    if (cur == 'children') return acc;
-    return acc + ' ' + camelCaseToDash(cur);
+    if (props[cur] && coolButtonClasses[cur])
+      return acc + ' ' + coolButtonClasses[cur];
+    return acc;
   }, 'button');
   return <button className={addedClass}>{props.children}</button>;
 };
