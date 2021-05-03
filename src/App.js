63c004c891b21dac8c9c404030b7a5305601552a
Marcia Faria <mfndnc@gmail.com>
2021-05-03 16:52:53 +0200

    iteraction 2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import 'bulma/css/bulma.css';
 import './App.css';
 import Navbar from './navbar/Navbar';
+import FormField from './formfield/FormField';
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
     </main>
   );
 };
