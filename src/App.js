39f0270a69a6d870bfa8cedfae3ea13eed565086
Tigran Harutyunyan <tigrandeboris@outlook.com>
2021-05-03 22:03:40 +0200

    ADD iteration 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Navbar from './navbar/Navbar';
 import 'bulma/css/bulma.css';
+import Navbar from './navbar/Navbar';
+import FormField from './formfield/FormField';
+
 
 class App extends React.Component {
   render() {
     return (
-        <Navbar />
+        <div>
+          <Navbar />
+          <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+          <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+        </div>
+
     )
   }
   }
