692af6387c91f1889aec390b7a0db039e622da97
Pablo del Barrio <pablodbb@gmail.com>
2021-05-03 22:24:58 +0200

    Iteration 4 done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
-import Navbar from './navbar/Navbar';
-import FormField from './formfield/FormField'
-import CoolButton from './coolbutton/CoolButton'
+import Signup from './signup/Signup';
 
 const App = () => {
-  return (
-  <div>
-  <Navbar />
-
-    <div className="form">
-      <FormField label="Name" type="text" placeholder="e.g Papá Pitufo" />
-      <FormField label="Email" type="email" placeholder="e.g. papapitufo@gmail.com" />
-      <CoolButton isSuccess className="is-rounded"> Login </CoolButton>
-    </div>
-  </div>
+  return (  
+  <Signup />   
   )
 };
 
