7268a96586dff6d0a7b727072e7d5a338c6e0758
silvia rodriguez <silrodcar@gmail.com>
2021-05-06 18:05:55 +0200

    iteration 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import Navbar from './navbar/Navbar'
+import Navbar from './navbar/Navbar';
+import FormField from './formField/FormField';
 
 
 
 const App = () => {
-  return <Navbar /> ;
+  return(
+  <div>
+  <Navbar />
+  <FormField 
+    label={'Name'} 
+    type={'text'} 
+    placeholder={'e.g Mayhem Rodriguez'}/>
+
+  <FormField 
+  label={'Email'} 
+  type={'email'} 
+  placeholder={'Mayhem@world.com'}/> 
+
+</div>
+  ) 
+
 };
 
 export default App;
