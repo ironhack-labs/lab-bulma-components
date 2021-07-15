54de4f625711c2a0c70cb0e6d99a74e7695094ef
Deat <debora2013aguiartavares@gmail.com>
2021-07-15 19:24:36 -0300

    nav bar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import 'bulma/css/bulma.css'
 import Navbar from "./navbar/Navbar"
+import FormField from './formfield/FormField';
+
 
 const App = () => {
-  return <div>
+  return (
     <Navbar/>
-  </div>
+  )
 };
 
 export default App;
