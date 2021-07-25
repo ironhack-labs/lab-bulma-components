15a864d92090c6c88e6f6ba987965aea0965ff31
Flavio-Pestana <flaviopestan@gmail.com>
2021-07-25 20:42:28 -0300

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+
+import 'bulma/css/bulma.css'
+import Navbar from './navbar/Navbar.js';
+import FormField from './formfield/FormField.js';
+import CoolButton from './button/Button.js'
+
 const App = () => {
-  return <h1>Bulma</h1>;
+  return <h1>Signup</h1>;
 };
 
 export default App;
