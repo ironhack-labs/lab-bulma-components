0d81c0513d0b3ab0494b295c3d228dab361cb34b
Arthur Melo <arthurmelo1000@gmail.com>
2021-07-31 11:54:31 -0300

    interation 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import NavBar from './NavBar/NavBar.js';
+import 'bulma/css/bulma.css'
+
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <>
+      <NavBar />
+    </>
+  )
 };
 
 export default App;
