1f4447f1dc64bc6e494a658c6eed1b707cd56297
Renan Bartalo <renan.bartalo@outlook.com>
2021-09-02 22:53:53 -0300

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Message from "./message/Message";
 import Signup from "./signup/Signup";
+import Container from "./container/Container";
 
 const App = () => {
   return (
-    <main className="container">
+    <Container> 
       <Signup />
       <Message title = "Hello World"/> 
-    </main>
+    </Container> 
   );
 };
 
---
+++ src/container/Container.js
@@ -0,0 +1,13 @@
+import React from "react";
+import 'bulma/css/bulma.css';
+
+const Container = (props) => {
+    return (
+<div className="container">
+{props.children}
+</div>
+    );
+  };
+  
+  export default Container;
+  
