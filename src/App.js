795a6e7783c41ee75aba36a23535863f9b453b3a
eduardo brandiao <eduardo.brandiao.adm@gmail.com>
2021-09-07 18:22:42 -0300

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import Navbar from './Navbar/navbar';
+import FormField from './Formfield/formfield';
+import CoolButton from './CoolButton/CoolButton';
+
+
+// JSX version
+
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <div>
+      <Navbar />;
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+      <FormField label="Senha" type="password" placeholder="digite uma senha" />
+      <CoolButton isSmall isDanger className="is-rounded my-class">Button 1</CoolButton>
+    </div>
+  )
 };
 
 export default App;
---
+++ src/CoolButton/CoolButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bulma/css/bulma.css';
+
+const CoolButton = props => {
+    return (
+        <div>
+            <button class="button is-rounded my-class is-danger is-small">Button 1</button>
+            <button class="button is-small is-success">Button 2</button>
+        </div>
+    );
+};
+export default CoolButton;
