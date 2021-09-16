0f4be9a274190671e35276765661fe38bb445114
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-09-16 11:45:01 +0200

    add basic set up with create react app

---
+++ bulma-components/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './navbar/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
