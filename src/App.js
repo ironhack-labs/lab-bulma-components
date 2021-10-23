2b7e6eee2e19e756b1fb062144a20dcae742debc
CherylHuang1988 <cherylhuang1988@gmail.com>
2021-10-23 20:57:59 +0200

    Done with the lab

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./navbar/Navbar";
+import FormField from "./formfield/FormField";
+import CoolButton from "./coolbutton/CoolButton";
 
+//fragments
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <>
+      <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
+      <FormField label="Password" type="password" placeholder="password" />
+
+      <button className="button is-small is-danger is-rounded myy-class">
+        Log in
+      </button>
+      <button className="button is-small is-success">Sigh up</button>
+    </>
+  );
 };
 
 export default App;
