9c14a024d4cc098f65f8657d62f2490816c2e2ff
Philipp Kastl <picastle@posteo.de>
2022-01-10 23:49:31 +0100

    iteration 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import Navbar from './navbar/Navbar'
-import './App.css'
+import React from "react";
+import Navbar from "./navbar/Navbar";
+import FormField from "./formfield/FormField";
+import "./App.css";
 
 const App = () => {
-  return <Navbar />
+  return (
+    <div>
+      <Navbar />
+      <FormField label="Name" type='text' placeholder='Peter'/>
+      <FormField
+        label="Email" type='email' placeholder='abc@abc.de'
+      />
+    </div>
+  );
 };
 
 export default App;
