d9937f9d8cb7e507ef814be5f91fca28c2b79264
Adalberto Sampaio <sampaio.afn@gmail.com>
2022-01-11 22:02:49 -0300

    first iteration done

---
+++ src/navbar/Navbar.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import 'bulma/css/bulma.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar is-transparent">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="/">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            alt="Bulma: a modern CSS framework based on Flexbox"
+            width="112"
+            height="28"
+          />
+        </a>
+        <div
+          className="navbar-burger"
+          data-target="navbarExampleTransparentExample"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
+      <div id="navbarExampleTransparentExample" className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item" href="/">
+            Home
+          </a>
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="field is-grouped">
+              <p className="control">
+                <a
+                  className="bd-tw-button button"
+                  data-social-network="Twitter"
+                  data-social-action="tweet"
+                  data-social-target="https://bulma.io"
+                  href="/"
+                >
+                  <span>Login</span>
+                </a>
+              </p>
+              <p className="control">
+                <a className="button is-primary" href="/">
+                  <span>Signup</span>
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
