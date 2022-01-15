4e26f9c044ce95edd5217c8c9ee563fa42360eb3
Michael Seitz <michael.s.seitz@gmail.com>
2022-01-15 13:43:59 +0100

    iteration 1 & 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import NavBar from "./navbar/Navbar.js";
+import FormField from "./formfield/Formfield.js";
+// import './App.css';
 
-const App = () => {
-  return <h1>Bulma</h1>;
-};
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
