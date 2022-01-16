6945f9485a2cac72e72b5678f53760da0354fccb
Philipp Kastl <picastle@posteo.de>
2022-01-16 17:45:17 +0100

    add bulma button classes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Navbar from "./navbar/Navbar";
 import FormField from "./formfield/FormField";
+import CoolButton from './coolbutton/CoolButton'
 import "./App.css";
+import 'bulma/css/bulma.css'
 
 const App = () => {
+
   return (
     <div>
       <Navbar />
@@ -11,6 +14,8 @@ const App = () => {
       <FormField
         label="Email" type='email' placeholder='abc@abc.de'
       />
+      <CoolButton isSuccess>Success</CoolButton>
+      <CoolButton isRounded isDanger>Danger</CoolButton>
     </div>
   );
 };
