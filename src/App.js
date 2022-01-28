c4ca7028d4e0ce4fb497c6c036ddcf8284212c8a
Matheus Benamor <matheusqueirozbenamor@gmail.com>
2022-01-28 17:10:00 +0000

    Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Navbar from './navbar/Navbar';
 
 const App = () => {
-  return <h1>Bulma</h1>;
+ return (
+    <div>
+      <Navbar/>
+    </div> 
+  )
 };
 
 export default App;
